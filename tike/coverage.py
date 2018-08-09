7f5b23978ebe7e0ca34504cb5604e108bb99d24c
Daniel Ching <carterbox@users.noreply.github.com>
2018-08-09 18:14:52 -0500

    API: Update coverage function to new API

--- tike/coverage.py
+++ tike/coverage.py
@@ -68,55 +68,76 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-def coverage(grid_min, grid_size, ngrid, theta, h, v, line_weight=None,
-             anisotropy=1):
-    """Back-project lines over a grid.
+def coverage(object_grid, object_min, object_size,
+             probe_grid, probe_size, theta, h, v,
+             dwell=None):
+    """Return a coverage map using this probe.
 
-    Coverage will be calculated on a grid covering the range
-    `[grid_min, grid_min + grid_size)`.
+    The intersection between each line and each pixel is approximated by
+    the product of `line_width**2` and the length of segment of the
+    line segment `alpha` which passes through the pixel along the line.
 
     Parameters
     ----------
-    grid_min : tuple float (z, x, y)
-        The min corner of the grid.
-    grid_size : tuple float (z, x, y)
-        The side lengths of the grid along each dimension.
-    ngrid : tuple int (z, x, y)
-        The number of grid spaces along each dimension.
-    theta, h, v : (M, ) :py:class:`numpy.array`
-        The h, v, and theta coordinates of lines to back-project over
-        the grid.
-    line_weight : (M, ) :py:class:`numpy.array`
+    theta, h, v [radians, cm]
+        The positions of the min_corner of the probe.
+    dwell : (M, ) :py:class:`numpy.array` [s]
         Multiply the intersections lengths of the pixels and each line by these
         weights.
 
     Returns
     -------
-    coverage_map : :py:class:`numpy.ndarray` [length * line_weight]
+    coverage_map : :py:class:`numpy.ndarray` [s]
         An array of shape (ngrid, anisotropy) containing the sum of the
         intersection lengths multiplied by the line_weights.
+        A discretized map of the approximated procedure coverage.
     """
-    grid_min = utils.as_float32(grid_min)
-    grid_size = utils.as_float32(grid_size)
-    ngrid = utils.as_int32(ngrid)
-    assert np.all(grid_size > 0), "Grid dimensions must be > 0"
-    assert np.all(ngrid > 0), "Number of grid lines must be > 0"
-    h = utils.as_float32(h)
-    v = utils.as_float32(v)
-    theta = utils.as_float32(theta)
-    if line_weight is None:
-        line_weight = np.ones(theta.shape, dtype=np.float32)
-    line_weight = utils.as_float32(line_weight)
-    assert theta.size == h.size == v.size == line_weight.size, "line_weight," \
-        " theta, h, v must be the same size"
-    if anisotropy > 1:
-        coverage_map = np.zeros(list(ngrid) + [anisotropy], dtype=np.float32)
+    object_grid, object_min, object_size, probe_grid, probe_size, theta, h, v \
+        = _tomo_interface(object_grid, object_min, object_size,
+                          probe_grid, probe_size, theta, h, v)
+    ngrid = object_grid.shape
+    assert ngrid.size == 4, "Coverage map must have 4 dimensions."
+    # Multiply the trajectory by size of probe_grid
+    th1 = np.repeat(theta, dh.size)
+    dh, dv = line_offsets(probe_grid, probe_size)
+    h1 = np.repeat(h, dh.size).reshape(h.size, dh.size) + dh
+    v1 = np.repeat(v, dv.size).reshape(v.size, dv.size) + dv
+    if dwell is None:
+        dw1 = np.ones(th1.shape)
     else:
-        coverage_map = np.zeros(ngrid, dtype=np.float32)
-        anisotropy = 1
-    logging.info(" coverage {:,d} element grid".format(coverage_map.size))
-    externs.c_coverage(grid_min[0], grid_min[1], grid_min[2],
-                       grid_size[0], grid_size[1], grid_size[2],
-                       ngrid[0], ngrid[1], ngrid[2], anisotropy,
-                       theta, h, v, line_weight, h.size, coverage_map)
-    return coverage_map
+        dw1 = np.repeat(dwell, dv.size)
+    # Computer other parameters for c funcion
+    line_area = np.prod(probe_size / probe_grid.shape)
+    pixel_volume = np.prod(object_size / object_grid.shape[0:3])
+    weights = dw1 * line_area / pixel_volume  # [s m^2 / m^3]
+    logging.info(" coverage {:,d} element grid".format(object_grid.size))
+    # Send data to c function
+    externs.c_coverage(object_min[0], object_min[1], object_min[2],
+                       object_size[0], object_size[1], object_size[2],
+                       ngrid[0], ngrid[1], ngrid[2], ngrid[3],
+                       th1, h1, v1, weights, th1.size, object_grid)
+    return object_grid
+
+
+def line_offsets(probe_grid, probe_size):
+    """Generate h, v line offsets from the min corner and filter
+    zero-weighted lines.
+
+    Returns
+    -------
+    dh, dv : (N, ) np.array [cm]
+        The offsets in the horizontal and vertical directions
+    """
+    # Generate a grid of offset vectors
+    gh = (np.linspace(0, probe_size[0], probe_grid.shape[0], endpoint=False)
+          + probe_size[0] / probe_grid.shape[0] / 2)
+    gv = (np.linspace(0, probe_size[1], probe_grid.shape[1], endpoint=False)
+          + probe_size[1] / probe_grid.shape[1] / 2)
+    dh, dv = np.meshgrid(gh, gv, indexing='ij')
+    # Remove zero values
+    nonzeros = probe_grid != 0
+    h = dh[nonzeros].flatten()
+    v = dv[nonzeros].flatten()
+    logger.info(" probe uses {:,d} lines".format(h.size))
+    assert dv.size == dh.size
+    return h, v
