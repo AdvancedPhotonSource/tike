382a8656c138c4170d01e0f7e633cc95770b2335
Daniel Ching <carterbox@users.noreply.github.com>
2018-08-13 11:30:45 -0500

    API: Rename tike.probe to tike.trajectory remove Probe class
    The Probe class is useless since the API change where probes are
    represented as arrays, so removing it. However this module had
    useful other functions related to discretizing trajectories, so
    renamed the module.

--- tests/test_trajectory.py
+++ tests/test_trajectory.py
@@ -2,9 +2,9 @@
 # -*- coding: utf-8 -*-
 
 # #########################################################################
-# Copyright (c) 2017-2018, UChicago Argonne, LLC. All rights reserved.    #
+# Copyright (c) 2016, UChicago Argonne, LLC. All rights reserved.         #
 #                                                                         #
-# Copyright 2018. UChicago Argonne, LLC. This software was produced       #
+# Copyright 2015. UChicago Argonne, LLC. This software was produced       #
 # under U.S. Government contract DE-AC02-06CH11357 for Argonne National   #
 # Laboratory (ANL), which is operated by UChicago Argonne, LLC for the    #
 # U.S. Department of Energy. The U.S. Government has rights to use,       #
@@ -46,21 +46,51 @@
 # POSSIBILITY OF SUCH DAMAGE.                                             #
 # #########################################################################
 
-"""
-This file tells import which submodules exist in the `tike` namespace and what
-functions to import when someone calls `import tike`. Note that `tike.externs`
-and `tike.utils` are not imported here because they are not part of the public
-API.
-"""
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
 
-__version__ = '0.2.0'
-
-from tike.coverage import *
-from tike.ptycho import *
-from tike.scan import *
-from tike.tomo import *
+import numpy as np
+import matplotlib.pyplot as plt
 from tike.trajectory import *
-from tike.view import *
-import logging
 
-logging.getLogger(__name__).addHandler(logging.NullHandler())
+__author__ = "Daniel Ching"
+__copyright__ = "Copyright (c) 2016, UChicago Argonne, LLC."
+__docformat__ = 'restructuredtext en'
+
+
+def test_discrete_trajectory():
+    def stationary(t):
+        """Probe is stationary at location h = 8, v = 8"""
+        return 0*t, 8 + 0*t, 8 + 0*t
+
+    answer = discrete_trajectory(stationary,
+                                 tmin=0, tmax=0.65, xstep=0.1, tstep=1)
+    truth = ([0], [8], [8], [0.65], [0])
+    np.testing.assert_equal(answer, truth)
+
+
+def test_coded_exposure():
+    c_time = np.arange(11)
+    c_dwell = np.ones(11) * 0.5
+
+    time = np.array([  -1, 0.8, 1.8,   3, 4.1, 4.2, 6.1, 7.5, 8.6, 8.9, 8.9, 8.9, 20, 21])
+    dwell = np.array([0.1, 0.2, 0.4, 0.5, 0.1, 0.1, 0.6, 0.2, 0.2,   2,   0, 0.3,  1 , 1])
+
+    theta = np.arange(time.size)
+    h = np.arange(time.size)
+    v = np.arange(time.size)
+
+    th1, h1, v1, t1, d1, b1 = coded_exposure(theta, h, v, time, dwell, c_time,
+                                             c_dwell)
+
+    np.testing.assert_equal(th1, [2, 3, 4, 5, 6, 9, 11, 9])
+    np.testing.assert_equal(h1, [2, 3, 4, 5, 6, 9, 11, 9])
+    np.testing.assert_equal(v1, [2, 3, 4, 5, 6, 9, 11, 9])
+    np.testing.assert_equal(t1,     [2. ,  3. ,  4.1,  4.2,  6.1,  9. , 9. , 10. ])
+    np.testing.assert_allclose(d1, [ 0.2,  0.5,  0.1,  0.1,  0.4,  0.5, 0.2,  0.5])
+    np.testing.assert_equal(b1, [0, 1, 2, 4, 5, 7])
+
+
+if __name__ == '__main__':
+    test_discrete_trajectory()
+    test_coded_exposure()
