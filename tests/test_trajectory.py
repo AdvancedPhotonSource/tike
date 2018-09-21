3ee035fd19493e0afc36dc9a20d48a104fbb9e23
Daniel Ching <carterbox@users.noreply.github.com>
2018-09-21 18:48:11 +1000

    BLD: Activate all build targets

--- tests/test_trajectory.py
+++ tests/test_trajectory.py
@@ -50,7 +50,6 @@ from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
 import numpy as np
-import matplotlib.pyplot as plt
 from tike.trajectory import *
 
 __author__ = "Daniel Ching"
