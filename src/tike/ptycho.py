9d92d0ded0c1263d246802a70cf97f7a56b7a58c
Daniel Ching <carterbox@users.noreply.github.com>
2019-04-25 18:19:03 -0500

    NEW: Add conjugate gradient method

--- src/tike/ptycho.py
+++ src/tike/ptycho.py
@@ -315,7 +315,6 @@ def grad(
     epsilon : float
         Primal residual absolute termination criterion.
         TODO:@Selin Create better description
-
     """
     if not (np.iscomplexobj(psi) and np.iscomplexobj(probe)
             and np.iscomplexobj(reg)):
@@ -340,6 +339,7 @@ def grad(
         )  # yapf: disable
         # Updates for each illumination patch
         grad = _backward(
+            # FIXME: Divide by zero occurs when probe is all zeros?
             farplane - data / np.conjugate(farplane),
             probe=probe, v=v, h=h,
             psi_shape=psi.shape, psi_corner=psi_corner,
@@ -347,6 +347,110 @@ def grad(
         )  # yapf: disable
         # grad -= rho * (reg - psi + lamda / rho)
         psi = psi - grad
+    return psi
+
+
+def line_search(f, x, d, step_length=1, step_shrink=0.5):
+    """Return a new step_length using a backtracking line search.
+
+    https://en.wikipedia.org/wiki/Backtracking_line_search
+
+    Parameters
+    ----------
+    f : function(x)
+        The function being optimized.
+    x : vector
+        The current position.
+    d : vector
+        The search direction.
+    """
+    assert step_shrink < 1
+    assert step_shrink > 0
+    m = 0.5  # Some tuning parameter for termination
+    # Decrease the step length while the step moves us away from the minimum
+    while f(x + step_length * d) > f(x) + step_shrink * m:
+        if step_length < 1e-32: return step_length
+        step_length *= step_shrink
+    return step_length
+
+
+def cgrad(
+        data,
+        probe, v, h,
+        psi, psi_corner,
+        reg=0j, num_iter=1, gamma=0.25, eta=None,
+        **kwargs
+):  # yapf: disable
+    """Use conjugate gradient to estimate `psi`.
+
+    Parameters
+    ----------
+    gamma : float
+        The ptychography gradient descent step size.
+    eta : () :py:class:`numpy.array` complex
+        The search direction.
+    """
+    if not (np.iscomplexobj(psi) and np.iscomplexobj(probe)
+            and np.iscomplexobj(reg)):
+        raise TypeError("psi, probe, and reg must be complex.")
+    data = data.astype(np.float32)
+    probe = probe.astype(np.complex64)
+    psi = psi.astype(np.complex64)
+    # Compute weights for updates from each illumination
+    update_weights = combine_grids(
+        grids=np.tile(np.abs(probe)[np.newaxis, ...], [len(data), 1, 1]),
+        v=v, h=h,
+        combined_shape=psi.shape,
+        combined_corner=psi_corner,
+    )  # yapf: disable
+    update_weights[update_weights == 0] = 1
+    detector_shape = data.shape[1:]
+    # Define the function that we are minimizing
+    def maximum_a_posteriori_probability(psi):
+        """Return the probability that psi is correct given the data."""
+        simdata = _forward(
+            detector_shape,
+            probe=probe, v=v, h=h,
+            psi=psi, psi_corner=psi_corner,
+        )  # yapf: disable
+        return np.sum(
+            np.square(np.abs(simdata)) - 2 * data * np.log(np.abs(simdata)))
+    for i in range(num_iter):
+        # Compute the gradient at the current location
+        farplane = _forward(
+            detector_shape,
+            probe=probe, v=v, h=h,
+            psi=psi, psi_corner=psi_corner,
+        )  # yapf: disable
+        # Updates for each illumination patch
+        grad = _backward(
+            # FIXME: Divide by zero occurs when probe is all zeros?
+            farplane - data / np.conjugate(farplane),
+            probe=probe, v=v, h=h,
+            psi_shape=psi.shape, psi_corner=psi_corner,
+            weights=update_weights,
+        )  # yapf: disable
+        # grad -= rho * (reg - psi + lamda / rho)
+        # Update the search direction, eta.
+        # eta and grad are the same shape as psi
+        if eta is None:
+            eta = -grad
+        else:
+            denominator = np.sum(np.conjugate(grad - grad0) * eta)
+            if denominator != 0:
+                # Use previous eta if previous (grad - grad0) is zero
+                eta = -grad + eta * np.square(
+                    np.linalg.norm(grad)) / denominator
+        # Update the step length, gamma
+        gamma = line_search(
+            f=maximum_a_posteriori_probability,
+            x=psi,
+            d=eta,
+            step_length=gamma,
+        )
+        # Update the guess for psi
+        psi = psi + gamma * eta
+        grad0 = grad
     return psi
 
 
@@ -461,6 +565,12 @@ def reconstruct(
     #   struct or array.
     if algorithm is "grad":
         new_psi = grad(data=data,
+                       probe=probe, v=v, h=h,
+                       psi=psi, psi_corner=psi_corner,
+                       num_iter=num_iter,
+                       **kwargs)  # yapf: disable
+    elif algorithm is "cgrad":
+        new_psi = cgrad(data=data,
                        probe=probe, v=v, h=h,
                        psi=psi, psi_corner=psi_corner,
                        num_iter=num_iter,
