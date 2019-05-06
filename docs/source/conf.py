79500e6cd467c3f2f4aef5d2e85a8ce71037ec8e
Daniel Ching <carterbox@users.noreply.github.com>
2019-05-06 15:40:19 -0500

    DOC: Fix ReadtheDocs build

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,7 +25,7 @@ from pkg_resources import get_distribution
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath('../../src'))
 
 
 # -- General configuration ------------------------------------------------
@@ -202,6 +202,7 @@ texinfo_documents = [
 autodoc_mock_imports = [
     'matplotlib',
     'matplotlib.pyplot',
+    'mpi4py',
     'numpy',
     'scipy',
     'scipy.ndimage.interpolate',
