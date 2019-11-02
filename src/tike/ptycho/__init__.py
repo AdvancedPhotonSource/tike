093c6d42c893a9a10122ae3cad22042c2c7768e4
Daniel Ching <carterbox@users.noreply.github.com>
2019-11-02 17:47:40 -0500

    NEW: Let other packages to provide a PtychoBackend
    Because:
    - We want tike to be modular and extendable
    - Prevents tike from becoming large
    - Allows tike to remain noarch (no compilation during build)

--- src/tike/ptycho/__init__.py
+++ src/tike/ptycho/__init__.py
@@ -11,9 +11,19 @@ import os
 # Search available entry points for requested backend. Must set the
 # PtychoBackend variable BEFORE importing the rest of the module.
 if "TIKE_PTYCHO_BACKEND" in os.environ:
-    # something = os.environ["TIKE_PTYCHO_BACKEND"]
-    # from something import something as PtychoBackend
-    raise ImportError("Cannot set custom backend yet.")
+    import pkg_resources
+    _ptycho_backend_options = {}
+    for _entry_point in pkg_resources.iter_entry_points('tike.PtychoBackend'):
+        _ptycho_backend_options[_entry_point.name] = _entry_point.load()
+    _requested_backend = os.environ["TIKE_PTYCHO_BACKEND"]
+    if _requested_backend in _ptycho_backend_options:
+        PtychoBackend = _ptycho_backend_options[_requested_backend]
+    else:
+        raise ImportError(
+            "Cannot set PtychoBackend to '{}'. "
+            "Available options are: {}".format(_requested_backend,
+                                               _ptycho_backend_options)
+        )
 else:
     from tike.ptycho._core.numpy import PtychoNumPyFFT as PtychoBackend
 
