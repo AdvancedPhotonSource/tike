85d46746beb3e03f9b69bc80722de849e3fb0880
Daniel Ching <carterbox@users.noreply.github.com>
2020-02-17 16:56:04 -0600

    DOC: Update operator module docs

--- src/tike/operators/__init__.py
+++ src/tike/operators/__init__.py
@@ -8,6 +8,12 @@ are based. In this way, multiple solvers (e.g. ePIE, gradient descent, SIRT)
 implemented in Python can share the same core operators and can be upgraded to
 better operators in the future.
 
+All operator methods should take NumPy arrays as inputs. This means that
+operators who offload work to specialized hardware (such as GPUs) are
+responsible for their own memory management. This is a design decision which was
+made because clients of the operators library should not need to be concerned
+about memory locality.
+
 """
 
 from .convolution import *
