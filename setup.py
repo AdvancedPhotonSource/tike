afcd0a15fce38d368965f3ff5428f75044db8eb3
Daniel Ching <carterbox@users.noreply.github.com>
2020-04-29 18:05:59 -0500

    BLD: Update metadata to include cu files and LICENSE

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setuptools.setup(
     name='tike',
     packages=setuptools.find_packages('src'),
     package_dir={'': 'src'},
+    include_package_data=True,
     setup_requires=['setuptools_scm', 'setuptools_scm_git_archive'],
     use_scm_version=True,
     author='Doga Gursoy',
