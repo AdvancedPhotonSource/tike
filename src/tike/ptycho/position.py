2765c0065dc04e2789ce7d2f259c367c3c8d9144
Daniel Ching <carterbox@users.noreply.github.com>
2020-05-04 14:04:12 -0500

    REF: Update Dwievdi position correction for CuPy and single mode

--- src/tike/ptycho/position.py
+++ src/tike/ptycho/position.py
@@ -5,7 +5,7 @@ import numpy as np
 logger = logging.getLogger(__name__)
 
 
-def lstsq(a, b):
+def lstsq(a, b, xp):
     """Return the least-squares solution for a @ x = b.
 
     This implementation, unlike np.linalg.lstsq, allows a stack of matricies to
@@ -16,6 +16,7 @@ def lstsq(a, b):
         x (...,    N)
 
     ...seealso:: https://github.com/numpy/numpy/issues/8720
+                 https://github.com/cupy/cupy/issues/3062
     """
     assert a.shape[:-1] == b.shape, (f"Leading dims of a {a.shape}"
                                      f"and b {b.shape} must be same!")
@@ -23,7 +24,9 @@ def lstsq(a, b):
     a = a.reshape(-1, *a.shape[-2:])
     b = b.reshape(-1, *b.shape[-1:], 1)
     aT = np.swapaxes(a, -1, -2)
-    x = np.linalg.pinv(aT @ a) @ aT @ b
+    x = xp.empty((a.shape[0], a.shape[-1], 1), dtype=a.dtype)
+    for i in range(a.shape[0]):
+        x[i] = np.linalg.pinv(aT[i] @ a[i]) @ aT[i] @ b[i]
     return x.reshape(*shape, a.shape[-1])
 
 
@@ -51,44 +54,37 @@ def update_positions_pd(operator, data, psi, probe, scan,
     https://doi.org/10.1016/j.ultramic.2018.04.004.
     """
     # step 1: the difference between measured and estimate intensity
-    farplane = operator.fwd(psi=psi, scan=scan, probe=probe)
-    intensity = np.square(
-        np.linalg.norm(
-            farplane.reshape(*data.shape[:2], -1, *data.shape[2:]),
-            ord=2,
-            axis=2,
-        ))
+    intensity = operator._compute_intensity(data, psi, scan, probe)
     dI = (data - intensity).reshape(*data.shape[:-2], np.prod(data.shape[-2:]))
 
-    # step 2: the partial derivatives of wavefront respect to position
-    dfarplane_dx = (farplane - operator.fwd(
-        psi=psi,
-        probe=probe,
-        scan=scan + np.array((0, dx), dtype='float32'),
-    )) / dx
-    dfarplane_dy = (farplane - operator.fwd(
-        psi=psi,
-        probe=probe,
-        scan=scan + np.array((dx, 0), dtype='float32'),
-    )) / dx
+    dI_dx, dI_dy = 0, 0
+    for m in range(probe.shape[-3]):
 
-    # step 3: the partial derivatives of intensity respect to position
-    dI_dx = 2 * np.sum(
-        np.real(dfarplane_dx * np.conj(farplane)).reshape(
-            *data.shape[:2], -1, *data.shape[2:]),
-        axis=2,
-    )
-    dI_dy = 2 * np.sum(
-        np.real(dfarplane_dy * np.conj(farplane)).reshape(
-            *data.shape[:2], -1, *data.shape[2:]),
-        axis=2,
-    )
+        # step 2: the partial derivatives of wavefront respect to position
+        farplane = operator.fwd(psi=psi, scan=scan, probe=probe[..., m:m+1, :, :])
+        dfarplane_dx = (farplane - operator.fwd(
+            psi=psi,
+            probe=probe[..., m:m+1, :, :],
+            scan=scan + operator.xp.array((0, dx), dtype='float32'),
+        )) / dx
+        dfarplane_dy = (farplane - operator.fwd(
+            psi=psi,
+            probe=probe[..., m:m+1, :, :],
+            scan=scan + operator.xp.array((dx, 0), dtype='float32'),
+        )) / dx
+
+        # step 3: the partial derivatives of intensity respect to position
+        dI_dx += 2 * np.real(dfarplane_dx * farplane.conj()).reshape(
+            *data.shape[:2], -1, *data.shape[2:])
+
+        dI_dy += 2 * np.real(dfarplane_dy * farplane.conj()).reshape(
+            *data.shape[:2], -1, *data.shape[2:])
 
     # step 4: solve for ΔX, ΔY using least squares
     dI_dxdy = np.stack((dI_dy.reshape(*dI.shape), dI_dx.reshape(*dI.shape)),
                        axis=-1)
 
-    grad = lstsq(a=dI_dxdy, b=dI)
+    grad = lstsq(a=dI_dxdy, b=dI, xp=operator.xp)
 
     logger.debug('grad max: %+12.5e min: %+12.5e', np.max(grad), np.min(grad))
     logger.debug('step size: %3.2g', step)
