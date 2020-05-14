5e777b6a67adf743f730af71b59c1a6eb9c62f32
Daniel Ching <carterbox@users.noreply.github.com>
2020-05-14 14:42:01 -0500

    REF: Refactor usfft.us2eq
    Removes one use of bincount.

--- src/tike/operators/numpy/usfft.py
+++ src/tike/operators/numpy/usfft.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def _get_kernel(xp, pad, mu):
     """Return the interpolation kernel for the USFFT."""
     xeq = xp.mgrid[-pad:pad, -pad:pad, -pad:pad]
@@ -38,6 +41,19 @@ def eq2us(f, x, n, eps, xp):
     cons = [xp.sqrt(xp.pi / mu)**3, -xp.pi**2 / mu]
     delta = lambda l, i, x: ((l - m + i).astype('float32') / (2 * n) - x)**2
 
+    # Vectorize approach (faster)
+    F = xp.zeros(x.shape[0], dtype="complex64")
+    ell = ((2 * n * x) // 1).astype(xp.int32)  # nearest grid to x
+    for i0 in range(2 * m):
+        delta0 = delta(ell[:, 0], i0, x[:, 0])
+        for i1 in range(2 * m):
+            delta1 = delta(ell[:, 1], i1, x[:, 1])
+            for i2 in range(2 * m):
+                delta2 = delta(ell[:, 2], i2, x[:, 2])
+                Fkernel = cons[0] * xp.exp(cons[1] * (delta0 + delta1 + delta2))
+                F += Fe[n + ell[:, 0] + i0, n + ell[:, 1] + i1,
+                        n + ell[:, 2] + i2] * Fkernel
+
     # # Sequential approach (slow)
     # F = xp.zeros(x.shape[0], dtype="complex64")
     # for k in range(x.shape[0]):
@@ -53,10 +69,41 @@ def eq2us(f, x, n, eps, xp):
     #                     + ((ell2 - m + i2) / (2 * n) - x[k, 2])**2
     #                 ))  # yapf: disable
     #                 F[k] += Fe[n + ell0 + i0, n + ell1 + i1, n + ell2 + i2] * kera
+    return F
+
+
+def us2eq(f, x, n, eps, xp):
+    """USFFT from unequally-spaced grid to equally-spaced grid.
+
+    Parameters
+    ----------
+    f : (n**3) complex64
+        Values of unequally-spaced function on the grid x
+    x : (n**3) float
+        The frequencies on the unequally-spaced grid
+    n : int
+        The size of the equall spaced grid.
+    eps : float
+        The accuracy of computing USFFT
+    """
+    ndim = 3
+    pad = n // 2  # where zero-padding stops
+    end = pad + n  # where f stops
+
+    # parameters for the USFFT transform
+    mu = -xp.log(eps) / (2 * n**2)
+    Te = 1 / xp.pi * xp.sqrt(-mu * xp.log(eps) + (mu * n)**2 / 4)
+    m = xp.int(xp.ceil(2 * n * Te))
+
+    # smearing kernel (ker)
+    kernel = _get_kernel(xp, pad, mu)
+
+    # smearing operation (G=f*kera)
+    cons = [xp.sqrt(xp.pi / mu)**3, -xp.pi**2 / mu]
+    delta = lambda l, i, x: ((l - m + i).astype('float32') / (2 * n) - x)**2
 
     # Vectorize approach (faster)
-    F = xp.zeros(x.shape[0], dtype="complex64")
-    Fe = Fe.ravel()
+    G = xp.zeros([(2 * (n + m))**ndim], dtype="complex64")
     ell = ((2 * n * x) // 1).astype(xp.int32)  # nearest grid to x
     stride = ((2 * (n + m))**2, 2 * (n + m))
     for i0 in range(2 * m):
@@ -71,86 +118,46 @@ def eq2us(f, x, n, eps, xp):
                     + stride[1] * (n + ell[:, 1] + i1)
                     + stride[0] * (n + ell[:, 0] + i0)
                 )  # yapf: disable
-                F += Fe[ids] * Fkernel
-
-    return F
-
-
-def us2eq(f, x, n, eps, xp):
-    """
-        USFFT from unequally-spaced grid to equally-spaced grid
-        x - unequally-spaced grid
-        f - function on the grid x
-        eps - accuracy of computing USFFT
-    """
-    # parameters for the USFFT transform
-    mu = -xp.log(eps) / (2 * n**2)
-    Te = 1 / xp.pi * xp.sqrt(-mu * xp.log(eps) + (mu * n)**2 / 4)
-    m = xp.int(xp.ceil(2 * n * Te))
-
-    # smearing kernel (ker)
-    ker = xp.zeros((2 * n, 2 * n, 2 * n), dtype="complex64")
-    [xeq0, xeq1, xeq2] = xp.mgrid[-n // 2:n // 2, -n // 2:n // 2,
-                                  -n // 2:n // 2]
-    ker[n // 2:n // 2 + n, n // 2:n // 2 + n,
-        n // 2:n // 2 + n] = xp.exp(-mu * xeq0**2 - mu * xeq1**2 - mu * xeq2**2)
-
-    # smearing operation (G=f*kera)
-    G = xp.zeros([(2 * n + 2 * m) * (2 * n + 2 * m) * (2 * n + 2 * m)],
-                 dtype="complex64")
-
-    # Sequential approach (slow)
-    # for k in range(x.shape[0]):
-    #     ell0 = xp.int(xp.floor(2*n*x[k, 0]))
-    #     ell1 = xp.int(xp.floor(2*n*x[k, 1]))
-    #     ell2 = xp.int(xp.floor(2*n*x[k, 2]))
-    #     for i0 in range(2*m):
-    #         for i1 in range(2*m):
-    #             for i2 in range(2*m):
-    #                 kera = xp.sqrt(xp.pi)**3/xp.sqrt(mu*mu*mu)*(xp.exp( -xp.pi**2/mu*((ell0-m+i0)/(2*n)-x[k, 0])**2
-    #                                                                     -xp.pi**2/mu*((ell1-m+i1)/(2*n)-x[k, 1])**2
-    #                                                                     -xp.pi**2/mu*((ell2-m+i2)/(2*n)-x[k, 2])**2))
-    #                 G[n+ell0+i0, n+ell1+i1, n+ell2+i2] += f[k]*kera
-
-    # Vectorize approach (faster)
-    ell0 = ((2 * n * x[:, 0]) // 1).astype(xp.int32)
-    ell1 = ((2 * n * x[:, 1]) // 1).astype(xp.int32)
-    ell2 = ((2 * n * x[:, 2]) // 1).astype(xp.int32)
-    # precomute constants
-    cons = [xp.sqrt(xp.pi / mu)**3, xp.pi**2 / mu]
-
-    for i0 in range(2 * m):
-        delta0 = (ell0 - m + i0).astype('float32') / (2 * n) - x[:, 0]
-        for i1 in range(2 * m):
-            delta1 = (ell1 - m + i1).astype('float32') / (2 * n) - x[:, 1]
-            for i2 in range(2 * m):
-                delta2 = (ell2 - m + i2).astype('float32') / (2 * n) - x[:, 2]
-
-                kera = cons[0] * xp.exp(-cons[1] *
-                                        (delta0**2 + delta1**2 + delta2**2))
-                ids = n+ell2+i2+(2*n+2*m)*(n+ell1+i1) + \
-                    (2*n+2*m)*(2*n+2*m)*(n+ell0+i0)
-                vals = f * kera
+                vals = f * Fkernel
                 # accumulate by indexes (with possible index intersections),
                 # TODO acceleration of bincount!!
-                vals = xp.bincount(ids, weights=vals.real) + \
-                    1j*xp.bincount(ids, weights=vals.imag)
+                vals = (xp.bincount(ids, weights=vals.real) +
+                        1j * xp.bincount(ids, weights=vals.imag))
                 ids = xp.nonzero(vals)[0]
                 G[ids] += vals[ids]
 
-    # wrapping array G
-    [idx, idy, idz] = xp.mgrid[-m:2 * n + m, -m:2 * n + m, -m:2 * n + m]
-    idx0 = xp.mod(idx + 2 * n, 2 * n)
-    idy0 = xp.mod(idy + 2 * n, 2 * n)
-    idz0 = xp.mod(idz + 2 * n, 2 * n)
-    ind = (idz0 + idy0 * (2 * n) + idx0 * (2 * n * 2 * n)).flatten()
-    # accumulate by indexes (with possible index intersections)
-    G = xp.bincount(ind, weights=G.real) + 1j * xp.bincount(ind, weights=G.imag)
-    G = G.reshape([2 * n, 2 * n, 2 * n])
+    G = _unpad(G.reshape([2 * (n + m)] * ndim), m)
 
     # FFT and compesantion for smearing
     F = xp.fft.fftshift(xp.fft.fftn(xp.fft.fftshift(G)))
-    F = F[n//2:n//2+n, n//2:n//2+n, n//2:n//2+n] / \
-        ker[n//2:n//2+n, n//2:n//2+n, n//2:n//2+n]/(2*n*2*n*2*n)
+    F = F[pad:end, pad:end, pad:end] / ((2 * n)**ndim * kernel)
 
     return F
+
+
+def _unpad(array, width, mode='wrap'):
+    """Remove padding from an array in-place.
+
+    Parameters
+    ----------
+    array : array
+        The array to strip.
+    width : int
+        The number of indices to remove from both sides along each dimension.
+    mode : string
+        'wrap' - Add the discarded regions to the array by wrapping them. The
+        end regions are added to the beginning and the beginning regions are
+        added the end of the new array.
+
+    Returns
+    -------
+    array : array
+        A view of the original array.
+    """
+    twice = 2 * width
+    for _ in range(array.ndim):
+        array[+width:+twice] += array[-width:]
+        array[-twice:-width] += array[:width]
+        array = array[width:-width]
+        array = np.moveaxis(array, 0, -1)
+    return array
