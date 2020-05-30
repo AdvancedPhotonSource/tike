c7ec2828469be610645bdea0267017defb95bb42
Daniel Ching <carterbox@users.noreply.github.com>
2020-05-29 19:46:50 -0500

    TST: Fix checkerboard test to run on CI

--- tests/operators/test_checkerboard.py
+++ tests/operators/test_checkerboard.py
@@ -1,53 +1,32 @@
 """Check whether the checkerboard algorithm is equivalent to fftshift."""
-import numpy as np
-import cupy as cp
-from cupyx.scipy.fft import fftn, ifftn
-from cupyx.scipy.fftpack import get_fft_plan
-from cupy.cuda import memory_hooks
+import numpy as xp
 
 from tike.operators.numpy.usfft import checkerboard
 
 
-def fftshift_two(a):
+def shifted_fft_two(a, xp):
     return checkerboard(
-        cp,
-        fftn(
-            checkerboard(cp, a),
+        xp,
+        xp.fft.fftn(
+            checkerboard(xp, a),
             norm='ortho',
-            overwrite_x=True,
         ),
         inverse=True,
     )
 
 
-def shifted_fft_ref(a):
-    return np.fft.ifftshift(np.fft.fftn(
-        np.fft.fftshift(a),
+def shifted_fft_ref(a, xp):
+    return xp.fft.ifftshift(xp.fft.fftn(
+        xp.fft.fftshift(a),
         norm='ortho',
     ))
 
 
-if __name__ == "__main__":
-    shape = np.random.randint(1, 32, 3) * 2
-    a = cp.random.rand(*shape) + 1j * cp.random.rand(*shape)
+def test_checkerboard_correctness():
+    shape = xp.random.randint(1, 32, 3) * 2
+    a = xp.random.rand(*shape) + 1j * xp.random.rand(*shape)
     a = a.astype('complex64')
     b = a.copy()
-    plan = get_fft_plan(a)
-
-    mempool = cp.get_default_memory_pool()
-
-    mempool.free_all_blocks()
-    hook = memory_hooks.LineProfileHook()
-    with hook, plan:
-        b = fftshift_two(b)
-    hook.print_report()
-
-    print()
-
-    mempool.free_all_blocks()
-    hook = memory_hooks.LineProfileHook()
-    with hook, plan:
-        a = shifted_fft_ref(a)
-    hook.print_report()
-
-    cp.testing.assert_array_almost_equal(a, b, decimal=5)
+    a = shifted_fft_ref(a, xp)
+    b = shifted_fft_two(b, xp)
+    xp.testing.assert_array_almost_equal(a, b, decimal=5)
