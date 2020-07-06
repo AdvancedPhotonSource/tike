7d47da5322d9358c56135aafb5daade3b8d3adc8
Daniel Ching <carterbox@users.noreply.github.com>
2020-07-06 18:02:48 -0500

    NEW: Enable wrapper for farneback's method from OpenCV

--- src/tike/align/solvers/farneback.py
+++ src/tike/align/solvers/farneback.py
@@ -18,24 +18,25 @@ def farneback(
     data,
     unaligned,
     pyr_scale=0.5,
-    levels=5,
-    winsize=13,
-    iterations=10,
+    levels=3,
+    winsize=19,
+    iterations=16,
     poly_n=5,
-    poly_sigma=1.1,
-    flags=None,
+    poly_sigma=4,
+    flags=4,
     flow=None,
     **kwargs,
 ):
     """Find the flow from unaligned to data using Farneback's algorithm
 
-    For parameter descriptions see https://docs.opencv.org/4.3.0/dc/d6b/group__video__track.html#ga5d10ebbd59fe09c5f650289ec0ece5af
+    For parameter descriptions see
+    https://docs.opencv.org/4.3.0/dc/d6b/group__video__track.html
 
     Parameters
     ----------
     data, unaligned (..., M, N)
         The images to be aligned.
-    flow : (..., M, N, 2) float
+    flow : (..., M, N, 2) float32
         The inital guess for the displacement field.
 
     References
@@ -54,14 +55,14 @@ def farneback(
 
     for i in range(len(data)):
         flow[i] = calcOpticalFlowFarneback(
-            *_rescale_8bit(data[i], unaligned[i]),
-            pyr_scale,
-            levels,
-            winsize,
-            iterations,
-            poly_n,
-            poly_sigma,
-            flags,
+            *_rescale_8bit(np.angle(data[i]), np.angle(unaligned[i])),
             flow=flow[i],
+            pyr_scale=pyr_scale,
+            levels=levels,
+            winsize=winsize,
+            iterations=iterations,
+            poly_n=poly_n,
+            poly_sigma=poly_sigma,
+            flags=flags,
         )
-    return flow.reshape(*shape, 2)
+    return {'shift': flow.reshape(*shape, 2), 'cost': -1}
