f29af01ee4b6f4830ecbfc9c80d36b0446ce130c
Daniel Ching <carterbox@users.noreply.github.com>
2020-07-15 14:31:08 -0500

    NEW: Add solver for recovering distorted image

--- src/tike/align/solvers/__init__.py
+++ src/tike/align/solvers/__init__.py
@@ -2,8 +2,10 @@
 
 from .cross_correlation import cross_correlation
 from .farneback import farneback
+from .cgrad import cgrad
 
 __all__ = [
     "cross_correlation",
     "farneback",
+    "cgrad",
 ]
---
+++ src/tike/align/solvers/cgrad.py
@@ -0,0 +1,35 @@
+import logging
+
+from tike.opt import conjugate_gradient
+
+logger = logging.getLogger(__name__)
+
+
+def cgrad(
+    op,
+    original,
+    unaligned,
+    flow,
+    num_iter=4,
+    reg=0, rho=0,
+    **kwargs
+):  # yapf: disable
+    """Recover an undistorted image from a given flow."""
+
+    def cost_function(original):
+        return op.xp.linalg.norm((op.fwd(original, flow) - unaligned).ravel())**2 + rho * op.xp.linalg.norm((original - reg).ravel())**2
+
+    def grad(original):
+        return op.fwd(op.fwd(original, flow) - unaligned, -flow) + rho * (original - reg)
+
+    cost = 0
+    original, cost = conjugate_gradient(
+        op.xp,
+        x=original,
+        cost_function=cost_function,
+        grad=grad,
+        num_iter=num_iter,
+    )
+
+    logger.info('%10s cost is %+12.5e', 'original', cost)
+    return {'original': original, 'cost': cost}
