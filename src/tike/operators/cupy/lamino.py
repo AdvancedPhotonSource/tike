4c60a1e2636eb5931dada186ab254ec3116203d1
Daniel Ching <carterbox@users.noreply.github.com>
2020-08-04 14:12:50 -0500

    API: Remove NumPy Operators
    In order to simplify the API, reduce maintainance, and reduce
    confusion(?); there is now only one operator implementation that
    uses CuPy. This decision was approved by @xiadong-yu @nikitinvv @slnsrydn
    @tekinbicer
    
    The consequence is that only Linux platforms are now supported (or
    Windows platforms that have a new enough version of CuPy).

--- src/tike/operators/cupy/lamino.py
+++ src/tike/operators/cupy/lamino.py
@@ -1,10 +1,12 @@
+__author__ = "Daniel Ching, Viktor Nikitin"
+__copyright__ = "Copyright (c) 2020, UChicago Argonne, LLC."
+
 from importlib_resources import files
 
 import cupy as cp
 from cupyx.scipy.fft import fft2, ifft2, fftn
 
-from tike.operators import numpy
-from tike.operators.numpy.usfft import eq2us, us2eq, checkerboard
+from .usfft import eq2us, us2eq, checkerboard
 from .operator import Operator
 
 _cu_source = files('tike.operators.cupy').joinpath('usfft.cu').read_text()
@@ -15,13 +17,42 @@ def _fftn(*args, **kwargs):
     return fftn(*args, **kwargs, overwrite_x=True)
 
 
-class Lamino(Operator, numpy.Lamino):
+class Lamino(Operator):
+    """A Laminography operator.
 
-    def __init__(self, *args, **kwargs):
-        super(Lamino, self).__init__(
-            *args,
-            **kwargs,
-        )
+    Laminography operators to simulate propagation of the beam through the
+    object for a defined tilt angle. An object rotates around its own vertical
+    axis, nz, and the beam illuminates the object some tilt angle off this
+    axis.
+
+    Attributes
+    ----------
+    n : int
+        The pixel width of the cubic reconstructed grid.
+    theta : array-like float32
+        The projection angles; rotation around the vertical axis of the object.
+    tilt : float32
+        The tilt angle; the angle between the rotation axis of the object and
+        the light source. π / 2 for conventional tomography. 0 for a beam path
+        along the rotation axis.
+
+    Parameters
+    ----------
+    u : (nz, n, n) complex64
+        The complex refractive index of the object. nz is the axis
+        corresponding to the rotation axis.
+    data : (ntheta, n, n) complex64
+        The complex projection data of the object.
+    """
+
+    def __init__(self, n, theta, tilt, eps=1e-3,
+                 **kwargs):  # noqa: D102 yapf: disable
+        """Please see help(Lamino) for more info."""
+        self.n = n
+        self.ntheta = len(theta)
+        self.tilt = tilt
+        self.eps = eps
+        self.xi = self._make_grids(theta)
 
     def __enter__(self):
         """Return self at start of a with-block."""
@@ -118,3 +149,31 @@ class Lamino(Operator, numpy.Lamino):
             const.astype('float32'),
         ))
         return F
+
+    def cost(self, data, obj):
+        "Cost function for the least-squres laminography problem"
+        return self.xp.linalg.norm((self.fwd(obj) - data).ravel())**2
+
+    def grad(self, data, obj):
+        "Gradient for the least-squares laminography problem"
+        return self.adj(data=self.fwd(obj) - data) / (self.ntheta * self.n**3)
+
+    def _make_grids(self, theta):
+        """Return (ntheta*n*n, 3) unequally-spaced frequencies for the USFFT."""
+        [kv, ku] = self.xp.mgrid[-self.n // 2:self.n // 2,
+                                 -self.n // 2:self.n // 2] / self.n
+        ku = ku.ravel().astype('float32')
+        kv = kv.ravel().astype('float32')
+        xi = self.xp.zeros([self.ntheta, self.n * self.n, 3], dtype='float32')
+        ctilt, stilt = self.xp.cos(self.tilt), self.xp.sin(self.tilt)
+        for itheta in range(self.ntheta):
+            ctheta = self.xp.cos(theta[itheta])
+            stheta = self.xp.sin(theta[itheta])
+            xi[itheta, :, 2] = ku * ctheta + kv * stheta * ctilt
+            xi[itheta, :, 1] = -ku * stheta + kv * ctheta * ctilt
+            xi[itheta, :, 0] = kv * stilt
+        # make sure coordinates are in (-0.5,0.5), probably unnecessary
+        xi[xi >= 0.5] = 0.5 - 1e-5
+        xi[xi < -0.5] = -0.5 + 1e-5
+
+        return xi.reshape(self.ntheta * self.n * self.n, 3)
--- src/tike/operators/cupy/propagation.py
+++ src/tike/operators/cupy/propagation.py
@@ -1,11 +1,91 @@
-import cupy as cp
-from cupyx.scipy.fft import fftn, ifftn
-from cupyx.scipy.fftpack import get_fft_plan
+"""Defines a free-space propagation operator based on the CuPy FFT module."""
 
-from tike.operators import numpy
+__author__ = "Daniel Ching, Viktor Nikitin"
+__copyright__ = "Copyright (c) 2020, UChicago Argonne, LLC."
+
 from .cache import CachedFFT
 from .operator import Operator
 
+import numpy as np
 
-class Propagation(Operator, CachedFFT, numpy.Propagation):
-    """A Fourier-based free-space propagation using CuPy."""
+class Propagation(CachedFFT, Operator):
+    """A Fourier-based free-space propagation using CuPy.
+
+    Take an (..., N, N) array and apply the Fourier transform to the last two
+    dimensions.
+
+    Attributes
+    ----------
+    detector_shape : int
+        The pixel width and height of the nearplane and farplane waves.
+    model : string
+        The type of noise model to use for the cost functions.
+    cost : (data-like, farplane-like) -> float
+        The function to be minimized when solving a problem.
+    grad : (data-like, farplane-like) -> farplane-like
+        The gradient of cost.
+
+    Parameters
+    ----------
+    nearplane: (..., detector_shape, detector_shape) complex64
+        The wavefronts after exiting the object.
+    farplane: (..., detector_shape, detector_shape) complex64
+        The wavefronts hitting the detector respectively.
+        Shape for cost functions and gradients is
+        (ntheta, nscan // fly, fly, 1, detector_shape, detector_shape).
+    data, intensity : (ntheta, nscan, detector_shape, detector_shape) complex64
+        data is the square of the absolute value of `farplane`. `data` is the
+        intensity of the `farplane`.
+
+    """
+
+    def __init__(self, detector_shape, model='gaussian', **kwargs):
+        self.detector_shape = detector_shape
+        self.cost = getattr(self, f'_{model}_cost')
+        self.grad = getattr(self, f'_{model}_grad')
+
+    def fwd(self, nearplane, overwrite=False, **kwargs):
+        """Forward Fourier-based free-space propagation operator."""
+        self._check_shape(nearplane)
+        shape = nearplane.shape
+        return self._fft2(
+            nearplane.reshape(-1, self.detector_shape, self.detector_shape),
+            norm='ortho',
+            axes=(-2, -1),
+            overwrite=overwrite,
+        ).reshape(shape)
+
+    def adj(self, farplane, overwrite=False, **kwargs):
+        """Adjoint Fourier-based free-space propagation operator."""
+        self._check_shape(farplane)
+        shape = farplane.shape
+        return self._ifft2(
+            farplane.reshape(-1, self.detector_shape, self.detector_shape),
+            norm='ortho',
+            axes=(-2, -1),
+            overwrite=overwrite,
+        ).reshape(shape)
+
+    def _check_shape(self, x):
+        assert type(x) is self.xp.ndarray, type(x)
+        shape = (-1, self.detector_shape, self.detector_shape)
+        if (__debug__ and x.shape[-2:] != shape[-2:]):
+            raise ValueError(f'waves must have shape {shape} not {x.shape}.')
+
+    # COST FUNCTIONS AND GRADIENTS --------------------------------------------
+
+    def _gaussian_cost(self, data, intensity):
+        return np.linalg.norm(np.ravel(np.sqrt(intensity) - np.sqrt(data)))**2
+
+    def _gaussian_grad(self, data, farplane, intensity, overwrite=False):
+        return farplane * (
+            1 - np.sqrt(data) / (np.sqrt(intensity) + 1e-32)
+        )[:, :, np.newaxis, np.newaxis]  # yapf:disable
+
+    def _poisson_cost(self, data, intensity):
+        return np.sum(intensity - data * np.log(intensity + 1e-32))
+
+    def _poisson_grad(self, data, farplane, intensity, overwrite=False):
+        return farplane * (
+            1 - data / (intensity + 1e-32)
+        )[:, :, np.newaxis, np.newaxis]  # yapf: disable
