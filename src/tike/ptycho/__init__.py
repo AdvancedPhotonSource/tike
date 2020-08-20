65e8c40bf0af0536cfbb4f3475e0a936041b3f8f
Daniel Ching <carterbox@users.noreply.github.com>
2020-08-20 14:43:07 -0500

    DOC: Remove references to TIKE_BACKEND in docs

--- src/tike/ptycho/__init__.py
+++ src/tike/ptycho/__init__.py
@@ -1,8 +1,3 @@
-"""Provide ptychography solvers and tooling.
-
-Select a non-default Ptycho implementation by setting the TIKE_BACKEND
-environment variable.
-
-"""
+"""Provide ptychography solvers and tooling."""
 from .ptycho import *
 from .position import check_allowed_positions
