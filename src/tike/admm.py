963d9e06bb9d8ada8019c0e8954a09136d30c12c
Daniel Ching <carterbox@users.noreply.github.com>
2020-09-24 19:21:19 -0500

    DEV: Before pad rotate as official operator

--- src/tike/admm.py
+++ src/tike/admm.py
@@ -12,13 +12,18 @@ import tike.lamino
 import tike.ptycho
 
 
-def update_penalty(psi, h, h0, rho):
+def update_penalty(comm, psi, h, h0, rho):
     r = np.linalg.norm(psi - h)**2
     s = np.linalg.norm(rho * (h - h0))**2
-    if (r > 10 * s):
-        rho *= 2
-    elif (s > 10 * r):
-        rho *= 0.5
+    r, s = [comm.gather(x) for x in ([r], [s])]
+    if comm.rank == 0:
+        r = np.sum(r)
+        s = np.sum(s)
+        if (r > 10 * s):
+            rho *= 2
+        elif (s > 10 * r):
+            rho *= 0.5
+    rho = comm.broadcast(rho)
     logging.info(f"Update penalty parameter ρ = {rho}.")
     return rho
 
@@ -37,7 +42,7 @@ def find_min_max(data):
 
     return mmin, mmax
 
-
+# lamino + align converges (ish) according to Viktor, Doga when looking at lagrangian
 def lamino_align(
     data,
     psi,
@@ -276,28 +281,125 @@ def lamino_align(
     return u
 
 
+# lamino by itself works converges
+def lamino(
+    data,
+    psi,
+    scan,
+    probe,
+    theta,
+    tilt,
+    u=None,
+    flow=None,
+    niter=1,
+    folder=None,
+):
+    """Solve the joint lamino-alignment problem using ADMM.
+
+    Parameters
+    ----------
+    data : (ntheta, detector, detector) float32
+    tilt : radians float32
+        The laminography tilt angle in radians.
+    theta : float32
+        The rotation angle of each data frame in radians.
+    u : (detector, detector, detector) complex64
+        An initial guess for the object
+    lamd : (ntheta, detector, detector) float32
+    flow : (ntheta, detector, detector, 2) float32
+        An initial guess for the alignmnt displacement field.
+    """
+    comm = MPICommunicator()
+
+    all_theta = comm.gather(theta)
+    if comm.rank == 0:
+        np.save(f"{folder}/alltheta", all_theta)
+
+    with cp.cuda.Device(comm.rank):
+
+        if comm.rank == 0:
+            all_trimmed = dxchange.read_tiff(
+                f'{folder}/trimmed-real-{(1):03d}.tiff'
+            ) + 1j * dxchange.read_tiff(f'{folder}/trimmed-imag-{(1):03d}.tiff')
+            phi = all_trimmed.astype('complex64')
+
+        for k in range(niter):
+
+            if comm.rank == 0:
+                logging.info('Solve the laminography problem.')
+
+                lresult = tike.lamino.reconstruct(
+                    data=-1j * np.log(phi),
+                    theta=all_theta,
+                    tilt=tilt,
+                    obj=u,
+                    algorithm='cgrad',
+                    num_iter=1,
+                )
+                u = lresult['obj']
+
+                Hu = np.exp(1j * tike.lamino.simulate(
+                    obj=u,
+                    tilt=tilt,
+                    theta=all_theta,
+                ))
+
+                print(
+                    f"k: {k:03d}, "
+                    "lamino: {:+6.3e}".format(lresult['cost']),
+                    flush=True,
+                )
+                dxchange.write_tiff(
+                    Hu.real,
+                    f'{folder}/Hu-real-{(k+1):03d}.tiff',
+                    dtype='float32',
+                )
+                dxchange.write_tiff(
+                    Hu.imag,
+                    f'{folder}/Hu-imag-{(k+1):03d}.tiff',
+                    dtype='float32',
+                )
+                dxchange.write_tiff(
+                    u.real,
+                    f'{folder}/particle-real-{(k+1):03d}.tiff',
+                    dtype='float32',
+                )
+                dxchange.write_tiff(
+                    u.imag,
+                    f'{folder}/particle-imag-{(k+1):03d}.tiff',
+                    dtype='float32',
+                )
+
+    return u
+
+
 def rotate_and_crop(x, corners=None, radius=128, angle=72.035):
     """Rotate x in two trailing dimensions then crop around center-of-mass.
 
     Parameters
     ----------
     x : (M, N, O) complex64
+        The image to be cropped.
     radius : int
+        How much to crop around the center-of-mass along each dimension.
     angle : float
         Rotation angle in degrees.
+    corners : (len(x), 2) float32
+        Crop at these positions instead of the center-of-mass.
     """
     rotate_params = dict(
         angle=angle,
         clip=False,
         preserve_range=True,
         resize=False,
+        mode='edge',
     )
     corners = -np.ones((len(x), 2), dtype=int) if corners is None else corners
     patch = np.zeros((len(x), 2 * radius, 2 * radius), dtype='complex64')
     for i in range(len(x)):
         # Rotate by desired angle (degrees)
-        x[i].real = skimage.transform.rotate(x[i].real, **rotate_params, cval=1)
-        x[i].imag = skimage.transform.rotate(x[i].imag, **rotate_params, cval=0)
+        x[i].real = skimage.transform.rotate(x[i].real, **rotate_params)
+        x[i].imag = skimage.transform.rotate(x[i].imag, **rotate_params)
 
         if corners[i][0] < 0:
             # Find the center of mass
@@ -327,18 +429,20 @@ def rotate_and_crop(x, corners=None, radius=128, angle=72.035):
 
 
 def uncrop_and_rotate(x, patch, lo, radius=128, angle=-72.035):
+    lo = np.zeros((len(x), 2), dtype=int) if lo is None else lo
     rotate_params = dict(
         angle=angle,
         clip=False,
         preserve_range=True,
         resize=False,
+        mode='edge',
     )
     for i in range(len(x)):
         x[i][lo[i][0]:lo[i][0] + 2 * radius,
              lo[i][1]:lo[i][1] + 2 * radius] = patch[i]
         # Rotate by desired angle (degrees)
-        x[i].real = skimage.transform.rotate(x[i].real, **rotate_params, cval=1)
-        x[i].imag = skimage.transform.rotate(x[i].imag, **rotate_params, cval=0)
+        x[i].real = skimage.transform.rotate(x[i].real, **rotate_params)
+        x[i].imag = skimage.transform.rotate(x[i].imag, **rotate_params)
     return x
 
 
@@ -353,31 +457,34 @@ def ptycho_lamino_align(
     flow=None,
     niter=1,
     folder=None,
+    fixed_crop=False,
 ):
     """Solve the joint ptycho-lamino-alignment problem using ADMM."""
     comm = MPICommunicator()
     with cp.cuda.Device(comm.rank):
         # Set preliminary values for ADMM
-        w = 256
+        w = 256 + 32
         flow = np.zeros(
             [len(theta), w, w, 2],
             dtype='float32',
         ) if flow is None else flow
         winsize = w
+        corners = None
 
         u = np.zeros(
             [w, w, w],
             dtype='complex64',
         ) if u is None else u
+
         phi = np.exp(1j * tike.lamino.simulate(
             obj=u,
             tilt=tilt,
             theta=theta,
-        ))
-        phi = phi.astype('complex64')
+        )).astype('complex64')
+
         Hu = phi.copy()
-        Hu0 = Hu
-        Dφ0 = phi
+        Hu0 = phi.copy()
+        Dφ0 = phi.copy()
 
         presult = {  # ptychography result
             'psi': psi,
@@ -385,8 +492,6 @@ def ptycho_lamino_align(
             'probe': probe,
         }
 
-        corners = None
-        reg_p = None
         λ_p = np.zeros_like(phi)
         ρ_p = 0.5
         λ_a = np.zeros_like(phi)
@@ -398,13 +503,14 @@ def ptycho_lamino_align(
             logging.info("Solve the ptychography problem.")
 
             if k > 0:
-                # Skip regularization on zeroth iteration because we don't know
-                # value of the cropping corner locations
                 reg_p = uncrop_and_rotate(
-                    psi_rotated,
-                    λ_p / ρ_p - Dφ,
-                    corners,
+                    x=psi_rotated,
+                    patch=λ_p / ρ_p - Dφ,
+                    lo=corners,
+                    radius=w // 2,
                 )
+            else:
+                reg_p = None
             presult = tike.ptycho.reconstruct(
                 data=data,
                 reg=reg_p,
@@ -421,7 +527,11 @@ def ptycho_lamino_align(
             psi = presult['psi']
 
             logging.info("Rotate and crop projections.")
-            psi_rotated, trimmed, corners = rotate_and_crop(psi.copy(), corners)
+            psi_rotated, trimmed, corners = rotate_and_crop(
+                x=psi.copy(),
+                corners=corners if fixed_crop else None,
+                radius=w // 2,
+            )
 
             logging.info("Recover aligned projections from unaligned.")
             aresult = tike.align.reconstruct(
@@ -430,18 +540,14 @@ def ptycho_lamino_align(
                 flow=flow,
                 num_iter=4,
                 algorithm='cgrad',
-                reg=np.exp(1j * tike.lamino.simulate(
-                    obj=u,
-                    tilt=tilt,
-                    theta=theta,
-                )) - λ_a / ρ_a,
+                reg=Hu - λ_a / ρ_a,
                 rho_p=ρ_p,
                 rho_a=ρ_a,
             )
             phi = aresult['original']
 
             logging.info("Estimate alignment using Farneback.")
-            aresult = tike.align.solvers.farneback(
+            fresult = tike.align.solvers.farneback(
                 op=None,
                 unaligned=trimmed + λ_p / ρ_p,
                 original=phi,
@@ -451,11 +557,10 @@ def ptycho_lamino_align(
                 winsize=winsize,
                 num_iter=4,
             )
+            flow = fresult['shift']
 
             # Gather all to one thread
-            λ_a, phi, theta, Hu_ = [
-                comm.gather(x) for x in (λ_a, phi, theta, Hu)
-            ]
+            λ_a, phi, theta = [comm.gather(x) for x in (λ_a, phi, theta)]
 
             if comm.rank == 0:
                 logging.info('Solve the laminography problem.')
@@ -483,16 +588,6 @@ def ptycho_lamino_align(
                     f'{folder}/phi-imag-{(k+1):03d}.tiff',
                     dtype='float32',
                 )
-                dxchange.write_tiff(
-                    Hu_[order].real,
-                    f'{folder}/Hu-real-{(k+1):03d}.tiff',
-                    dtype='float32',
-                )
-                dxchange.write_tiff(
-                    Hu_[order].imag,
-                    f'{folder}/Hu-imag-{(k+1):03d}.tiff',
-                    dtype='float32',
-                )
 
             # Separate again to multiple threads
             λ_a, phi, theta = [comm.scatter(x) for x in (λ_a, phi, theta)]
@@ -511,8 +606,8 @@ def ptycho_lamino_align(
             λ_p += ρ_p * CψDφ
             λ_a += ρ_a * φHu
 
-            ρ_p = update_penalty(phi, Hu, Hu0, ρ_p)
-            ρ_a = update_penalty(trimmed, Dφ, Dφ0, ρ_a)
+            ρ_p = update_penalty(comm, trimmed, Dφ, Dφ0, ρ_p)
+            ρ_a = update_penalty(comm, phi, Hu, Hu0, ρ_a)
             Hu0 = Hu
             Dφ0 = Dφ
 
@@ -523,28 +618,31 @@ def ptycho_lamino_align(
                     ρ_p * np.linalg.norm(CψDφ.ravel())**2
                 ],
                 [
-                    2 * np.sum(np.real(λ_a.conj() * φHu)) +
+                    2 * np.real(λ_a.conj() * φHu) +
                     ρ_a * np.linalg.norm(φHu.ravel())**2
                 ],
             )
             lagrangian = [comm.gather(x) for x in lagrangian]
+            acost = comm.gather([aresult['cost']])
 
             if comm.rank == 0:
                 lagrangian = [np.sum(x) for x in lagrangian]
                 print(
                     f"k: {k:03d}, ρ_p: {ρ_p:6.3e}, ρ_a: {ρ_a:6.3e}, "
                     f"winsize: {winsize:03d}, "
+                    f"alignment: {np.sum(acost):+6.3e} "
+                    f"laminography: {lresult['cost']:+6.3e} "
                     'Lagrangian: {:+6.3e} = {:+6.3e} {:+6.3e} {:+6.3e}'.format(
                         np.sum(lagrangian), *lagrangian),
                     flush=True,
                 )
                 dxchange.write_tiff(
-                    presult['psi'].real,
+                    trimmed.real,
                     f'{folder}/psi-real-{(k+1):03d}.tiff',
                     dtype='float32',
                 )
                 dxchange.write_tiff(
-                    presult['psi'].imag,
+                    trimmed.imag,
                     f'{folder}/psi-imag-{(k+1):03d}.tiff',
                     dtype='float32',
                 )
@@ -558,11 +656,214 @@ def ptycho_lamino_align(
                     f'{folder}/particle-imag-{(k+1):03d}.tiff',
                     dtype='float32',
                 )
+                dxchange.write_tiff(
+                    Hu.real,
+                    f'{folder}/Hu-real-{(k+1):03d}.tiff',
+                    dtype='float32',
+                )
+                dxchange.write_tiff(
+                    Hu.imag,
+                    f'{folder}/Hu-imag-{(k+1):03d}.tiff',
+                    dtype='float32',
+                )
                 np.save(f"{folder}/flow-tike-{(k+1):03d}", flow)
 
             # Limit winsize to larger value. 20?
             if winsize > 20:
                 winsize -= 1
+
+    result = presult
+    return result
+
+# Does not converge!?
+def ptycho_lamino(
+    data,
+    psi,
+    scan,
+    probe,
+    theta,
+    tilt,
+    u=None,
+    flow=None,
+    niter=1,
+    folder=None,
+    fixed_crop=True,
+):
+    """Solve the joint ptycho-lamino-alignment problem using ADMM."""
+    comm = MPICommunicator()
+    with cp.cuda.Device(comm.rank):
+        # Set preliminary values for ADMM
+        w = 256 + 32
+        corners = None
+
+        u = np.zeros(
+            [w, w, w],
+            dtype='complex64',
+        ) if u is None else u
+
+        Hu0 = np.exp(1j * tike.lamino.simulate(
+            obj=u,
+            tilt=tilt,
+            theta=theta,
+        )).astype('complex64')
+        TPHu0 = uncrop_and_rotate(
+            x=np.zeros_like(psi) + 1.0,
+            patch=Hu0,
+            lo=corners,
+            radius=w // 2,
+        )
+
+        presult = {  # ptychography result
+            'psi': psi,
+            'scan': scan,
+            'probe': probe,
+        }
+
+        λ_p = np.zeros_like(psi)
+        ρ_p = 0.5
+
+        for k in range(niter):
+            logging.info(f"Start ADMM iteration {k}.")
+
+            logging.info("Solve the ptychography problem.")
+
+            if k > 0:
+                reg_p = λ_p / ρ_p - TPHu
+            else:
+                reg_p = None
+            presult = tike.ptycho.reconstruct(
+                data=data,
+                reg=reg_p,
+                rho=ρ_p,
+                algorithm='combined',
+                num_iter=1,
+                cg_iter=4,
+                recover_psi=True,
+                recover_probe=True,
+                recover_positions=False,
+                model='gaussian',
+                **presult,
+            )
+            psi = presult['psi']
+
+            logging.info("Rotate and crop projections.")
+            _, trimmed, corners = rotate_and_crop(
+                x=psi + λ_p / ρ_p,
+                corners=corners if fixed_crop else None,
+                radius=w // 2,
+            )
+
+            # Gather all to one thread
+            trimmed, theta = [comm.gather(x) for x in (trimmed, theta)]
+
+            if comm.rank == 0:
+                logging.info('Solve the laminography problem.')
+                lresult = tike.lamino.reconstruct(
+                    data=-1j * np.log(trimmed),
+                    theta=theta,
+                    tilt=tilt,
+                    obj=u,
+                    algorithm='cgrad',
+                    num_iter=1,
+                    cg_iter=4,
+                )
+                u = lresult['obj']
+
+                # We cannot reorder phi, theta without ruining correspondence
+                # with data, psi, etc, but we can reorder the saved array
+                order = np.argsort(theta)
+                dxchange.write_tiff(
+                    trimmed[order].real,
+                    f'{folder}/phi-real-{(k+1):03d}.tiff',
+                    dtype='float32',
+                )
+                dxchange.write_tiff(
+                    trimmed[order].imag,
+                    f'{folder}/phi-imag-{(k+1):03d}.tiff',
+                    dtype='float32',
+                )
+
+            # Separate again to multiple threads
+            trimmed, theta = [comm.scatter(x) for x in (trimmed, theta)]
+            u = comm.broadcast(u)
+
+            logging.info('Update lambdas and rhos.')
+
+            Hu = np.exp(1j * tike.lamino.simulate(
+                obj=u,
+                tilt=tilt,
+                theta=theta,
+            ))
+            TPHu = uncrop_and_rotate(
+                x=np.zeros_like(psi) + 1.0,
+                patch=Hu,
+                lo=corners,
+                radius=w // 2,
+            )
+            ψTPHu = psi - TPHu
+            λ_p += ρ_p * ψTPHu
+
+            ρ_p = update_penalty(comm, psi, TPHu, TPHu0, ρ_p)
+            TPHu0 = TPHu
+
+            lagrangian = (
+                [presult['cost']],
+                [
+                    2 * np.real(λ_p.conj() * ψTPHu) +
+                    ρ_p * np.linalg.norm(ψTPHu.ravel())**2
+                ],
+            )
+            lagrangian = [comm.gather(x) for x in lagrangian]
+
+            if comm.rank == 0:
+                lagrangian = [np.sum(x) for x in lagrangian]
+                print(
+                    f"k: {k:03d}, ρ_p: {ρ_p:6.3e}, "
+                    f"laminography: {lresult['cost']:+6.3e} "
+                    'Lagrangian: {:+6.3e} = {:+6.3e} {:+6.3e}'.format(
+                        np.sum(lagrangian), *lagrangian),
+                    flush=True,
+                )
+                dxchange.write_tiff(
+                    psi.real,
+                    f'{folder}/psi-real-{(k+1):03d}.tiff',
+                    dtype='float32',
+                )
+                dxchange.write_tiff(
+                    psi.imag,
+                    f'{folder}/psi-imag-{(k+1):03d}.tiff',
+                    dtype='float32',
+                )
+                dxchange.write_tiff(
+                    u.real,
+                    f'{folder}/particle-real-{(k+1):03d}.tiff',
+                    dtype='float32',
+                )
+                dxchange.write_tiff(
+                    u.imag,
+                    f'{folder}/particle-imag-{(k+1):03d}.tiff',
+                    dtype='float32',
+                )
+                dxchange.write_tiff(
+                    TPHu.real,
+                    f'{folder}/TPHu-real-{(k+1):03d}.tiff',
+                    dtype='float32',
+                )
+                dxchange.write_tiff(
+                    TPHu.imag,
+                    f'{folder}/TPHu-imag-{(k+1):03d}.tiff',
+                    dtype='float32',
+                )
+                dxchange.write_tiff(
+                    (λ_p / ρ_p).imag,
+                    f'{folder}/lamb-imag-{(k+1):03d}.tiff',
+                    dtype='float32',
+                )
+                dxchange.write_tiff(
+                    (λ_p / ρ_p).real,
+                    f'{folder}/lamb-real-{(k+1):03d}.tiff',
+                    dtype='float32',
+                )
 
     result = presult
     return result
