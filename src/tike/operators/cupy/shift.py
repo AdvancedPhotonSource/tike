f87bc405c2f6115a94fdd2f5fbaa6e7d30b8ea83
Daniel Ching <carterbox@users.noreply.github.com>
2020-10-01 20:53:52 -0500

    API: Remove padding from Shift operator

--- src/tike/operators/cupy/shift.py
+++ src/tike/operators/cupy/shift.py
@@ -20,20 +20,17 @@ class Shift(CachedFFT, Operator):
 
         """
         shape = a.shape
-        a = a.reshape(-1, *shape[-2:])
-        pz, pn = a.shape[-2] // 2, a.shape[-1] // 2
-        padded = self.xp.pad(a, ((0, 0), (pz, pz), (pn, pn)))
-        [x, y] = self.xp.meshgrid(
-            self.xp.fft.fftfreq(2 * pn + a.shape[-1]).astype('float32'),
-            self.xp.fft.fftfreq(2 * pz + a.shape[-2]).astype('float32'),
+        padded = a.reshape(-1, *shape[-2:])
+        padded = self._fft2(padded, axes=(-2, -1), overwrite=overwrite)
+        x, y = self.xp.meshgrid(
+            self.xp.fft.fftfreq(padded.shape[-1]).astype('float32'),
+            self.xp.fft.fftfreq(padded.shape[-2]).astype('float32'),
         )
-        shift = self.xp.exp(
+        padded *= self.xp.exp(
             -2j * self.xp.pi *
             (x * shift[..., 1, None, None] + y * shift[..., 0, None, None]))
-        padded = self._fft2(padded, axes=(-2, -1), overwrite=overwrite)
-        padded *= shift
-        padded = self._ifft2(padded, axes=(-2, -1), overwrite=overwrite)
-        return padded[..., pz:-pz, pn:-pn].reshape(shape)
+        padded = self._ifft2(padded, axes=(-2, -1), overwrite=True)
+        return padded.reshape(*shape)
 
     def adj(self, a, shift, overwrite=False):
         return self.fwd(a, -shift, overwrite=overwrite)
