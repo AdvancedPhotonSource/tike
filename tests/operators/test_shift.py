96bcdbc81d8bc13baa00b932c59b8b0b53684618
Daniel Ching <carterbox@users.noreply.github.com>
2020-10-07 17:52:45 -0500

    TST: Refactor adjoint operator tests and add normalization test

--- tests/operators/test_shift.py
+++ tests/operators/test_shift.py
@@ -4,55 +4,35 @@
 import unittest
 
 import numpy as np
-
 from tike.operators import Shift
-from .util import random_complex, inner_complex
+
+from .util import random_complex, OperatorTests
 
 __author__ = "Daniel Ching, Viktor Nikitin"
 __copyright__ = "Copyright (c) 2020, UChicago Argonne, LLC."
 __docformat__ = 'restructuredtext en'
 
 
-class TestShift(unittest.TestCase):
+class TestShift(unittest.TestCase, OperatorTests):
     """Test the Shift operator."""
 
     def setUp(self, n=16, nz=17, ntheta=8):
-        """Load a dataset for reconstruction."""
-        self.n = n
-        self.nz = nz
-        self.ntheta = ntheta
-        print(Shift)
-
-    def test_adjoint(self):
-        """Check that the adjoint operator is correct."""
+        self.operator = Shift()
+        self.operator.__enter__()
+        self.xp = self.operator.xp
         np.random.seed(0)
-        shift = np.random.random([self.ntheta, 2])
-        original = random_complex(self.ntheta, self.nz, self.n)
-        data = random_complex(*original.shape)
-
-        with Shift() as op:
-
-            shift = op.asarray(shift, dtype='float32')
-            original = op.asarray(original, dtype='complex64')
-            data = op.asarray(data, dtype='complex64')
-
-            d = op.fwd(original, shift)
-            o = op.fwd(data, -shift)
-            original1 = op.fwd(d, -shift)
-
-            a = inner_complex(d, data)
-            b = inner_complex(original, o)
-            e = np.linalg.norm(original - original1) / np.linalg.norm(original)
-            print()
-            print('<Su,   a> = {:.6f}{:+.6f}j'.format(a.real.item(),
-                                                      a.imag.item()))
-            print('< u, S*a> = {:.6f}{:+.6f}j'.format(b.real.item(),
-                                                      b.imag.item()))
-            print(f'|u - S*Su| / |u|= {e.item():.6f}')
-
-            # Test whether Adjoint fixed probe operator is correct
-            op.xp.testing.assert_allclose(a.real, b.real, rtol=1e-5)
-            op.xp.testing.assert_allclose(a.imag, b.imag, rtol=1e-5)
+        self.m = self.xp.asarray(random_complex(ntheta, nz, n),
+                                 dtype='complex64')
+        self.m_name = 'a'
+        self.d = self.xp.asarray(random_complex(*self.m.shape),
+                                 dtype='complex64')
+        self.d_name = 'a'
+        self.kwargs = {
+            'shift':
+                self.xp.asarray((np.random.random([ntheta, 2]) - 0.5) * 7,
+                                dtype='float32')
+        }
+        print(self.operator)
 
 
 if __name__ == '__main__':
