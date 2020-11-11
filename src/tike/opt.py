4c5ddc046a082d4115a76844fe64a6ac4b8a1873
Daniel Ching <carterbox@users.noreply.github.com>
2020-11-11 15:51:13 -0600

    REF: Verbose logging of linesearch and reuse loss function

--- src/tike/opt.py
+++ src/tike/opt.py
@@ -23,6 +23,7 @@ def line_search(
     update_multi,
     step_length=1,
     step_shrink=0.5,
+    cost=None,
 ):
     """Return a new `step_length` using a backtracking line search.
 
@@ -38,6 +39,8 @@ def line_search(
         The initial step_length.
     step_shrink : float
         Decrease the step_length by this fraction at each iteration.
+    cost : float
+        f(x) if it is already known.
 
     Returns
     -------
@@ -45,6 +48,8 @@ def line_search(
         The optimal step length along d.
     cost : float
         The new value of the cost function after stepping along d.
+    x : float
+        The new value of x after stepping along d.
 
     References
     ----------
@@ -53,17 +58,27 @@ def line_search(
     """
     assert step_shrink > 0 and step_shrink < 1
     m = 0  # Some tuning parameter for termination
-    fx = f(x)  # Save the result of f(x) instead of computing it many times
+    # Save the result of f(x) instead of computing it many times
+    fx = f(x) if cost is None else cost
     # Decrease the step length while the step increases the cost function
+    step_count = 0
+    first_step = step_length
     while True:
-        fxsd = f(update_multi(x, step_length, d))
+        xsd = update_multi(x, step_length, d)
+        fxsd = f(xsd)
         if fxsd <= fx + step_shrink * m:
             break
         step_length *= step_shrink
         if step_length < 1e-32:
             warnings.warn("Line search failed for conjugate gradient.")
-            return 0, fx
-    return step_length, fxsd
+            step_length, fxsd, xsd = 0, fx, x
+            break
+        step_count += 1
+
+    logger.info("line_search: %d backtracks; %.3e -> %.3e; cost %.6e",
+                step_count, first_step, step_length, fxsd)
+
+    return step_length, fxsd, xsd
 
 
 def direction_dy(xp, grad0, grad1, dir_):
@@ -103,6 +118,8 @@ def conjugate_gradient(
     update_multi=update_single,
     num_iter=1,
     step_length=1,
+    num_search=None,
+    cost=None,
 ):
     """Use conjugate gradient to estimate `x`.
 
@@ -122,8 +139,13 @@ def conjugate_gradient(
         The updated subimages in all GPUs.
     num_iter : int
         The number of steps to take.
-
+    num_search : int
+        The number of during which to perform line search.
+    step_length : float
+        The initial multiplier of the search direction.
     """
+    num_search = num_iter if num_search is None else num_search
+
     for i in range(num_iter):
 
         grad1 = grad(x)
@@ -135,17 +157,20 @@ def conjugate_gradient(
 
         dir_list = dir_multi(dir_)
 
-        gamma, cost = line_search(
-            f=cost_function,
-            x=x,
-            d=dir_list,
-            update_multi=update_multi,
-            step_length=step_length,
-        )
+        if i < num_search:
+            step_length, cost, x = line_search(
+                f=cost_function,
+                x=x,
+                d=dir_list,
+                update_multi=update_multi,
+                step_length=step_length,
+                cost=cost,
+            )
+        else:
+            x = update_multi(x, step_length, dir_list)
+            logger.info("Blind update; length %.3e", step_length)
 
-        x = update_multi(x, gamma, dir_list)
-
-        logger.debug("step %d; length %.3e -> %.3e; cost %.6e", i, step_length,
-                     gamma, cost)
+    if __debug__ and num_search < num_iter:
+        cost = cost_function(x)
 
     return x, cost
