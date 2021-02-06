b1ac7d46c71096949bf058fe88bd12f649e27cde
Daniel Ching <carterbox@users.noreply.github.com>
2021-02-05 19:26:32 -0600

    API: Make laminography theta a parameter instead of constant
    Because:
    - This is required in order to divide the operator amongst
      multiple devices.

--- src/tike/lamino/solvers/cgrad.py
+++ src/tike/lamino/solvers/cgrad.py
@@ -8,29 +8,29 @@ logger = logging.getLogger(__name__)
 def cgrad(
     op,
     comm,
-    data, obj,
+    data, theta, obj,
     cg_iter=4,
     **kwargs
 ):  # yapf: disable
     """Solve the Laminogarphy problem using the conjugate gradients method."""
 
-    obj, cost = update_obj(op, comm, data, obj, num_iter=cg_iter)
+    obj, cost = update_obj(op, comm, data, theta, obj, num_iter=cg_iter)
 
     return {'obj': obj, 'cost': cost}
 
 
-def update_obj(op, comm, data, obj, num_iter=1):
+def update_obj(op, comm, data, theta, obj, num_iter=1):
     """Solver the object recovery problem."""
 
     def cost_function(obj):
-        cost_out = comm.pool.map(op.cost, data, obj)
+        cost_out = comm.pool.map(op.cost, data, theta, obj)
         if comm.use_mpi:
             return comm.Allreduce_reduce(cost_out, 'cpu')
         else:
             return comm.reduce(cost_out, 'cpu')
 
     def grad(obj):
-        grad_list = comm.pool.map(op.grad, data, obj)
+        grad_list = comm.pool.map(op.grad, data, theta, obj)
         if comm.use_mpi:
             return comm.Allreduce_reduce(grad_list, 'gpu')
         else:
