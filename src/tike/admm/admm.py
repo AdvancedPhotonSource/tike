64eb859212a6567cd7a2ef1b463d2029e6c0f833
Daniel Ching <carterbox@users.noreply.github.com>
2021-02-16 14:08:27 -0600

    REF: Update penalty params more often

--- src/tike/admm/admm.py
+++ src/tike/admm/admm.py
@@ -7,16 +7,26 @@ import tike.lamino
 import tike.ptycho
 
 
-def update_penalty(comm, psi, h, h0, rho, diff=10):
-    r = np.linalg.norm(psi - h)**2
-    s = np.linalg.norm(rho * (h - h0))**2
-    r, s = [comm.gather(x) for x in ([r], [s])]
+def update_penalty(comm, g, h, h0, rho, diff=4):
+    """Increase rho when L2 error between g and h becomes too large.
+
+    If rho is the penalty parameter associated with the constraint norm(y - x),
+    then rho is increased when
+
+        norm(g - h) > diff * rho^2 * norm(h - h0)
+
+    and decreased when
+
+        norm(g - h) * diff < rho^2 * norm(h - h0)
+
+    """
+    r = np.linalg.norm(g - h)**2
+    s = rho * rho * np.linalg.norm(h - h0)**2
+    r, s = [np.sum(comm.gather(x)) for x in ([r], [s])]
     if comm.rank == 0:
-        r = np.sum(r)
-        s = np.sum(s)
         if (r > diff * s):
             rho *= 2
-        elif (s > diff * r):
+        elif (r * diff < s):
             rho *= 0.5
     rho = comm.broadcast(rho)
     logging.info(f"Update penalty parameter ρ = {rho}.")
