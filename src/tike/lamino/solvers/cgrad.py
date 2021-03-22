9567f199db205b72af497d024545d4f076efd5cd
Daniel Ching <carterbox@users.noreply.github.com>
2021-03-22 11:37:44 -0500

    NEW: Add regularization to lamino cost function

--- src/tike/lamino/solvers/cgrad.py
+++ src/tike/lamino/solvers/cgrad.py
@@ -24,23 +24,68 @@ def _estimate_step_length(obj, theta, op):
     return 2 * scaler if op.xp.isfinite(scaler) else 1.0
 
 
-def _cost(data, theta, obj, op):
-    """Cost function for the least-squres laminography problem."""
-    return tike.linalg.norm(op.fwd(
+def _cost_tv(
+    data,
+    theta,
+    obj,
+    reg=0,
+    K=1,
+    penalty0=1,
+    penalty1=0,
+    op=None,
+    op1=None,
+):
+    """Cost function for the regularized laminography problem.
+
+    The cost function F(u) is a two term function as follows:
+
+    F(u) = penalty0 * norm(K * R(u) − data)**2
+         + penalty1 * norm(    J(u) − reg)**2
+
+    where
+
+    K = 1j / v * 2π * (ψ − dual0 / penalty0)
+    data = (ψ − dual0 / penalty0) * log(ψ − dual0/penalty0)
+    reg = ω − dual1 / penalty1
+
+    where ψ is the projection through the object and ω is J(obj) and v is the
+    wavenumber.
+
+    """
+    cost = penalty0 * tike.linalg.norm(K * op.fwd(
         u=obj,
         theta=theta,
     ) - data)**2
+    if penalty1 > 0:
+        cost += penalty1 * tike.linalg.norm(op1.fwd(obj) - reg)**2
+    return cost
 
 
-def _grad(data, theta, obj, op):
-    """Gradient for the least-squares laminography problem."""
-    return op.adj(
-        data=op.fwd(
-            u=obj,
-            theta=theta,
-        ) - data,
+def _grad_tv(
+    data,
+    theta,
+    obj,
+    reg=0,
+    K=1,
+    penalty0=1,
+    penalty1=0,
+    op=None,
+    op1=None,
+):
+    """Gradient for the regularized laminography problem.
+
+    ∇F(u) = penalty0 * R_adj(conj(K) * (K * R(u) − data))
+          + penalty1 * J_adj(               J(u) − reg)
+
+    """
+    Kconj = op.xp.conj(K) if K != 1 else K
+    grad = penalty0 * op.adj(
+        data=Kconj * (K * op.fwd(u=obj, theta=theta) - data),
         theta=theta,
     )
+    if penalty1 > 0:
+        grad += penalty1 * op1.adj(op1.fwd(obj) - reg)
+    return grad
 
 
 def cgrad(
@@ -78,14 +123,14 @@ def update_obj(op, comm, data, theta, obj, num_iter=1, step_length=1):
     """Solver the object recovery problem."""
 
     def cost_function(obj):
-        cost_out = comm.pool.map(_cost, data, theta, obj, op=op)
+        cost_out = comm.pool.map(_cost_tv, data, theta, obj, op=op)
         if comm.use_mpi:
             return comm.Allreduce_reduce(cost_out, 'cpu')
         else:
             return comm.reduce(cost_out, 'cpu')
 
     def grad(obj):
-        grad_list = comm.pool.map(_grad, data, theta, obj, op=op)
+        grad_list = comm.pool.map(_grad_tv, data, theta, obj, op=op)
         if comm.use_mpi:
             return comm.Allreduce_reduce(grad_list, 'gpu')
         else:
