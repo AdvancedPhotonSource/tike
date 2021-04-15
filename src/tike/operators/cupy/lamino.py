8311de49e66b4a2559e221c189b0fd5aa6051ee5
Daniel Ching <carterbox@users.noreply.github.com>
2021-04-14 19:56:56 -0500

    REF: Reduce memory-footprint of USFFT

--- src/tike/operators/cupy/lamino.py
+++ src/tike/operators/cupy/lamino.py
@@ -4,6 +4,7 @@ __copyright__ = "Copyright (c) 2020, UChicago Argonne, LLC."
 from importlib_resources import files
 
 import cupy as cp
+import numpy as np
 
 from .cache import CachedFFT
 from .usfft import eq2us, us2eq, checkerboard
@@ -44,8 +45,8 @@ class Lamino(CachedFFT, Operator):
                  **kwargs):  # noqa: D102 yapf: disable
         """Please see help(Lamino) for more info."""
         self.n = n
-        self.tilt = tilt
-        self.eps = eps
+        self.tilt = np.float32(tilt)
+        self.eps = np.float32(eps)
 
     def __enter__(self):
         """Return self at start of a with-block."""
@@ -124,16 +125,20 @@ class Lamino(CachedFFT, Operator):
         G = cp.zeros([2 * n] * 3, dtype="complex64")
         const = cp.array([cp.sqrt(cp.pi / mu)**3, -cp.pi**2 / mu],
                          dtype='float32')
+        assert G.dtype == cp.complex64
+        assert f.dtype == cp.complex64
+        assert x.dtype == cp.float32
+        assert const.dtype == cp.float32
         block = (min(self.scatter_kernel.max_threads_per_block, (2 * m)**3),)
         grid = (1, 0, min(f.shape[0], 65535))
         self.scatter_kernel(grid, block, (
             G,
-            f.astype('complex64'),
+            f,
             f.shape[0],
-            x.astype('float32'),
+            x,
             n,
             m,
-            const.astype('float32'),
+            const,
         ))
         return G
 
@@ -141,16 +146,20 @@ class Lamino(CachedFFT, Operator):
         F = cp.zeros(x.shape[0], dtype="complex64")
         const = cp.array([cp.sqrt(cp.pi / mu)**3, -cp.pi**2 / mu],
                          dtype='float32')
+        assert F.dtype == cp.complex64
+        assert Fe.dtype == cp.complex64
+        assert x.dtype == cp.float32
+        assert const.dtype == cp.float32
         block = (min(self.scatter_kernel.max_threads_per_block, (2 * m)**3),)
         grid = (1, 0, min(x.shape[0], 65535))
         self.gather_kernel(grid, block, (
             F,
-            Fe.astype('complex64'),
+            Fe,
             x.shape[0],
-            x.astype('float32'),
+            x,
             n,
             m,
-            const.astype('float32'),
+            const,
         ))
         return F
 
@@ -173,19 +182,19 @@ class Lamino(CachedFFT, Operator):
 
     def _make_grids(self, theta):
         """Return (ntheta*n*n, 3) unequally-spaced frequencies for the USFFT."""
-        [kv, ku] = self.xp.mgrid[-self.n // 2:self.n // 2,
-                                 -self.n // 2:self.n // 2] / self.n
-        ku = ku.ravel().astype('float32')
-        kv = kv.ravel().astype('float32')
+        u = self.xp.arange(-self.n // 2, self.n // 2, dtype='float32') / self.n
+        ku = self.xp.broadcast_to(u, (self.n, self.n)).ravel()
+        kv = self.xp.broadcast_to(u[:, None], (self.n, self.n)).ravel()
         xi = self.xp.zeros([theta.shape[-1], self.n * self.n, 3],
                            dtype='float32')
         ctilt, stilt = self.xp.cos(self.tilt), self.xp.sin(self.tilt)
+        ctheta, stheta = self.xp.cos(theta), self.xp.sin(theta)
+
         for itheta in range(theta.shape[-1]):
-            ctheta = self.xp.cos(theta[itheta])
-            stheta = self.xp.sin(theta[itheta])
-            xi[itheta, :, 2] = ku * ctheta + kv * stheta * ctilt
-            xi[itheta, :, 1] = -ku * stheta + kv * ctheta * ctilt
-            xi[itheta, :, 0] = kv * stilt
+            xi[itheta, :, 2] = +ku * ctheta[itheta] + kv * stheta[itheta] * ctilt
+            xi[itheta, :, 1] = -ku * stheta[itheta] + kv * ctheta[itheta] * ctilt
+        xi[:, :, 0] = kv * stilt
+
         # make sure coordinates are in (-0.5,0.5), probably unnecessary
         xi[xi >= 0.5] = 0.5 - 1e-5
         xi[xi < -0.5] = -0.5 + 1e-5
