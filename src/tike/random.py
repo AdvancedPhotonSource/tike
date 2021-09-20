044869860169d97f239baa65d3ad0fe02d323e15
Daniel Ching <carterbox@users.noreply.github.com>
2021-09-20 17:42:15 -0500

    REF: Move randomizer to random module

--- src/tike/random.py
+++ src/tike/random.py
@@ -3,10 +3,11 @@
 import cupy as cp
 import numpy as np
 
+randomizer = np.random.default_rng()
 
 def numpy_complex(*shape):
     """Return a complex random array in the range [-0.5, 0.5)."""
-    return (np.random.rand(*shape, 2) - 0.5).view('complex')[..., 0]
+    return (randomizer.rand(*shape, 2) - 0.5).view('complex')[..., 0]
 
 
 def cupy_complex(*shape):
