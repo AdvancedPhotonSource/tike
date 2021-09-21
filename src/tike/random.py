d4af9f68fede0cfb417b23c3d7479719a2399f88
Daniel Ching <carterbox@users.noreply.github.com>
2021-09-20 19:13:46 -0500

    BUG: Use correct function name for new random API

--- src/tike/random.py
+++ src/tike/random.py
@@ -7,7 +7,7 @@ randomizer = np.random.default_rng()
 
 def numpy_complex(*shape):
     """Return a complex random array in the range [-0.5, 0.5)."""
-    return (randomizer.rand(*shape, 2) - 0.5).view('complex')[..., 0]
+    return (randomizer.random(*shape, 2) - 0.5).view('complex')[..., 0]
 
 
 def cupy_complex(*shape):
