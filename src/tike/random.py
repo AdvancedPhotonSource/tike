a09bfad31a9dbb3d52f1de80fd6075333d97de63
Daniel Ching <carterbox@users.noreply.github.com>
2021-09-21 11:43:07 -0500

    BUG: Use proper syntax for randomizer

--- src/tike/random.py
+++ src/tike/random.py
@@ -5,9 +5,10 @@ import numpy as np
 
 randomizer = np.random.default_rng()
 
+
 def numpy_complex(*shape):
     """Return a complex random array in the range [-0.5, 0.5)."""
-    return (randomizer.random(*shape, 2) - 0.5).view('complex')[..., 0]
+    return (randomizer.random((*shape, 2)) - 0.5).view('complex')[..., 0]
 
 
 def cupy_complex(*shape):
