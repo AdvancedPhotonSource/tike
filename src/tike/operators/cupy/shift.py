b3e3974e43a6a534b18fbb5289deda70b562938d
Daniel Ching <carterbox@users.noreply.github.com>
2021-11-19 13:46:55 -0600

    MAI: Move all FFT calls to CuPy automated cache

--- src/tike/operators/cupy/shift.py
+++ src/tike/operators/cupy/shift.py
@@ -1,11 +1,11 @@
 __author__ = "Daniel Ching, Viktor Nikitin"
 __copyright__ = "Copyright (c) 2020, UChicago Argonne, LLC."
 
-from .cache import CachedFFT
+import cupyx.scipy.fft
 from .operator import Operator
 
 
-class Shift(CachedFFT, Operator):
+class Shift(Operator):
     """Shift last two dimensions of an array using Fourier method."""
 
     def fwd(self, a, shift, overwrite=False, cval=None):
@@ -23,7 +23,7 @@ class Shift(CachedFFT, Operator):
             return a
         shape = a.shape
         padded = a.reshape(-1, *shape[-2:])
-        padded = self._fft2(padded, axes=(-2, -1), overwrite=overwrite)
+        padded = cupyx.scipy.fft.fft2(padded, axes=(-2, -1), overwrite_x=overwrite)
         x, y = self.xp.meshgrid(
             self.xp.fft.fftfreq(padded.shape[-1]).astype('float32'),
             self.xp.fft.fftfreq(padded.shape[-2]).astype('float32'),
@@ -31,7 +31,7 @@ class Shift(CachedFFT, Operator):
         padded *= self.xp.exp(
             -2j * self.xp.pi *
             (x * shift[..., 1, None, None] + y * shift[..., 0, None, None]))
-        padded = self._ifft2(padded, axes=(-2, -1), overwrite=True)
+        padded = cupyx.scipy.fft.ifft2(padded, axes=(-2, -1), overwrite_x=True)
         return padded.reshape(*shape)
 
     def adj(self, a, shift, overwrite=False, cval=None):
