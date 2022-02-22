0f80f4f01bc0ac6529200b98d82169bb1c710652
Daniel Ching <carterbox@users.noreply.github.com>
2022-02-21 18:27:32 -0600

    Pretty confident that zernikes are correct now

--- src/tike/zernike.py
+++ src/tike/zernike.py
@@ -22,12 +22,15 @@ def Z(m: int, n: int, radius: np.array, angle: np.array) -> np.array:
     assert n >= 0, "Radial degree must be non-negative."
     _m_ = np.abs(m)
     assert _m_ <= n, "Angular frequency must be less than radial degree."
-    polynomial = R(_m_, n, radius) * np.exp(1j * m * angle)
-    polynomial[np.logical_or(radius < 0, radius > 1)] = np.nan
+    if m < 0:
+        polynomial = R(_m_, n, radius) * np.sin(m * angle)
+    else:
+        polynomial = R(_m_, n, radius) * np.cos(m * angle)
+    polynomial[np.logical_or(radius < 0, radius > 1)] = 0
     return polynomial
 
 
-def N(m: int, n: int) -> int:
+def N(m: int, n: int) -> float:
     """Zernike normalization factor."""
     return np.sqrt(2 * (n + 1) / (1 + (m == 0)))
 
@@ -35,32 +38,39 @@ def N(m: int, n: int) -> int:
 def R(m: int, n: int, radius: np.array) -> np.array:
     """Zernike radial polynomial."""
     # Initialize with k=0 case because this term will always be included
-    sign = 1
-    result = bino(n, m, 0) * radius**n
-    for k in range(1, n - m + 1):
+    sign = -1
+    result = 0 * radius
+    for k in range(0, (n - m) // 2 + 1):
         sign = -sign
-        result += sign * bino(n, m, k) * radius**(n - k)
+        result += sign * bino(n, m, k) * radius**(n - 2 * k)
     return result
 
 
 def bino(n: int, m: int, k: int) -> int:
     """Return the approximate binomial coeffient (a b)."""
     return int(
-        factorial(2 * n + 1 - k) / factorial(k) / factorial(n - m - k) /
-        factorial(n + m - k + 1))
+        factorial(n - k) / factorial(k) / factorial((n + m) // 2 - k) /
+        factorial((n - m) // 2 - k))
 
 
-def mode(size: int, n: int) -> np.array:
-    endpoint = 1.0 - 1 / (2 * size)
+def zernike_basis(size: int, degree: int) -> np.array:
+    """Return all circular Zernike basis for radial degree up to n."""
+    endpoint = 1.0 - 1.0 / (2 * size)
     x = np.linspace(-endpoint, endpoint, size, endpoint=True)
     coords = np.meshgrid(x, x, indexing='ij')
     radius = np.linalg.norm(coords, axis=0)
-    theta = np.arctan(coords[0] / coords[1])
+    theta = np.arctan2(coords[0], coords[1])
 
     basis = []
-    for _n in range(0, n):
-        for m in range(-_n, _n + 1):
-            basis.append(Z(m, n, radius, theta))
+    for m, n in valid_zernike_indices(degree):
+        basis.append(Z(m, n, radius, theta))
 
     basis = np.stack(basis, axis=0)
     return basis
+
+
+def valid_zernike_indices(degree):
+    for n in range(0, degree):
+        for m in range(-n, n + 1):
+            if (n - abs(m)) % 2 == 0:
+                yield m, n
--- tests/test_zernike.py
+++ tests/test_zernike.py
@@ -4,6 +4,7 @@ import os
 import tike.zernike
 import tike.view
 import matplotlib.pyplot as plt
+import numpy as np
 
 testdir = os.path.dirname(__file__)
 
@@ -14,8 +15,37 @@ class TestZernike(unittest.TestCase):
 
         fname = os.path.join(testdir, 'result', 'zernike')
         os.makedirs(fname, exist_ok=True)
-        for i, Z in enumerate(tike.zernike.mode(256, 3)):
+        for i, Z in enumerate(tike.zernike.zernike_basis(256, degree=10)):
             plt.figure()
-            tike.view.plot_complex(Z)
-            plt.savefig(os.path.join(fname, f"zernike-{i}.png"))
+            tike.view.plot_complex(Z, rmin=-1, rmax=1)
+            plt.savefig(os.path.join(fname, f"zernike-{i:02d}.png"))
             plt.close()
+
+    def _radial_template(self, m=0):
+        fname = os.path.join(testdir, 'result', 'zernike')
+        os.makedirs(fname, exist_ok=True)
+
+        radius = np.linspace(0, 1, 200)
+
+        plt.figure()
+        labels = []
+        for n in range(0, 9):
+            if (n + m) % 2 == 0:
+                v = tike.zernike.R(m, n, radius)
+                plt.plot(
+                    radius,
+                    v,
+                )
+                labels.append(n)
+        plt.legend(labels)
+        plt.savefig(os.path.join(fname, f"radial-function-{m}.png"))
+        plt.close()
+
+    def test_radial(self):
+        self._radial_template(0)
+
+    def test_radial_1(self):
+        self._radial_template(1)
+
+    def test_radial_2(self):
+        self._radial_template(2)
