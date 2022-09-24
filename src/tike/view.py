fixed the scaling issue so that the max and min phases aren't 0 and +/- pi

src/tike/view.py
@@ -91,27 +91,13 @@ def complexHSV_to_RGB(img0):
 
     hsv_img = np.ones((*sz, 3), 'float32')
 
-    hsv_img[ ..., 0 ] = np.angle( img0 )     
-    hsv_img[ ..., 2 ] = np.abs( img0 )
+    hsv_img[ ..., 0 ] = np.angle( img0 )    # always scaled between +/- pi   
+    hsv_img[ ..., 2 ] = np.abs( img0 )      # always scaled between 0 and +inf
 
     #================================
     # Rescale hue to the range [0, 1]
 
-    eps = np.finfo( np.float32 ).eps
-
-    hsv_img[ ..., 0 ] -= np.min( hsv_img[ ..., 0 ] )
-    hsv_img[ ..., 0 ] = hsv_img[ ..., 0 ] / ( eps + np.max( hsv_img[ ..., 0 ] ))
-    
-    #=================================================================
-    # NOTE: WHAT HAPPENS IF WE GIVE AN IMAGE WITH ONLY PHASE CONTRAST?
-    # THIS WILL SET MAGNITUDE TO ZERO EVERYWHERE...NOT WHAT WE WANT!!!
-    #
-    # I WILL ASSUME THAT THE USER WILL RESCALE THEIR ARRAY TO [ 0, 1 ]
-    # PRIOR TO CALLING THIS FUNCTION
-    #=================================================================
-    
-    # hsv_img[ ..., 2 ] -= np.min( hsv_img[ ..., 2 ] )
-    # hsv_img[ ..., 2 ] = hsv_img[ ..., 2 ] / ( eps + np.max( hsv_img[ ..., 2 ] ))
+    hsv_img[ ..., 0 ] = ( hsv_img[ ..., 0 ] + np.pi ) / ( 2 * np.pi )
 
     #==================================
     # convert HSV representation to RGB

tests/test_view.py
@@ -14,11 +14,13 @@ __author__ = "Ash Tripathi, "
 __copyright__ = "Copyright (c) 2018, UChicago Argonne, LLC."
 __docformat__ = 'restructuredtext en'
 
+###################################################################################################
 
-def create_colorwheel( nxny ):
+def create_color_wheel( nxny, Hmin = -np.pi, Hmax = +np.pi ):
 
-    #================================================================
-    # numerically create the value and hue components of a colorwheel
+    #=================================================================
+    # numerically create the value and hue components of a color wheel
+    #=================================================================
 
     nx, ny = nxny
 
@@ -27,59 +29,88 @@ def create_colorwheel( nxny ):
     V = np.sqrt( xx ** 2 + yy ** 2 )    
     V[ V > 1 ] = 0
 
-    H = ( np.arctan2( xx, yy ) + np.pi ) / ( np.pi * 2 )
+    H = np.arctan2( xx, yy ) 
+
+    H = np.angle( np.exp( 1j * (H - np.pi/2 )))  # global phase offset so that 0 and +\- pi are on the horizontal axis
+
+    #============================================
+    # zero out colors between certain phase range
+    #============================================
+
+    H[ ( H > Hmax ) | ( H < Hmin ) ] = 0
+
+    # plt.figure()
+    # plt.imshow( H )
+    # plt.show(block=False)
 
     #================================================================
     # assign the value and hue componenets to a ny x nx x 3 HSV image
+    #================================================================
 
     hsv_img = np.ones( ( ny, nx, 3 ), 'float32' )
 
-    hsv_img[ ..., 0 ] = H 
+    hsv_img[ ..., 0 ] = ( H + np.pi ) / ( np.pi * 2 )   # Rescale hue to the range [0, 1]
     hsv_img[ ..., 2 ] = V
 
     #==================================
     # convert HSV representation to RGB
+    #==================================
 
     rgb_img = mplcolors.hsv_to_rgb( hsv_img )
 
+    # plt.figure()
+    # plt.imshow( rgb_img )
+    # plt.show(block=False)
+
     return rgb_img, H, V
 
+###################################################################################################
 
-def create_colorrectangle( sxsy ):
+def create_color_rectangle( sxsy, Hmin = -np.pi, Hmax = +np.pi ):
 
-    #====================================================================
+    #=====================================================================
     # numerically create the value and hue components of a color rectangle
+    #=====================================================================
 
     szx, szy = sxsy
 
-    mag = np.tile( np.linspace( 0, 1, szx ), ( szy, 1 ) )
-    phs = np.linspace( -np.pi, np.pi, szy ).reshape( szy, 1 )
-    phs = np.tile( phs, ( 1, szx ))
+    V = np.tile( np.linspace( 0, 1, szx ), ( szy, 1 ) )
+
+    H = np.linspace( -np.pi, np.pi, szy ).reshape( szy, 1 )
+    H = np.tile( H, ( 1, szx ))
+
+    #============================================
+    # zero out colors between certain phase range
+    #============================================
+
+    H[ ( H > Hmax ) | ( H < Hmin ) ] = 0
+
+    # plt.figure()
+    # plt.imshow( H )
+    # plt.show(block=False)
 
     #================================================================
     # assign the value and hue componenets to a ny x nx x 3 HSV image
+    #================================================================
 
     hsv_img = np.ones( ( szy, szx, 3 ), 'float32' )
 
-    hsv_img[ ..., 0 ] = phs 
-    hsv_img[ ..., 2 ] = mag
-
-    #================================
-    # Rescale hue to the range [0, 1]
-
-    eps = np.finfo( np.float32 ).eps
-
-    hsv_img[ ..., 0 ] -= np.min( hsv_img[ ..., 0 ] )
-    hsv_img[ ..., 0 ] = hsv_img[ ..., 0 ] / ( eps + np.max( hsv_img[ ..., 0 ] ))
+    hsv_img[ ..., 0 ] = ( H + np.pi ) / ( np.pi * 2 )   # Rescale hue to the range [0, 1]
+    hsv_img[ ..., 2 ] = V
 
     #==================================
     # convert HSV representation to RGB
+    #==================================
 
     color_rectangle = mplcolors.hsv_to_rgb( hsv_img )
 
-    return color_rectangle, phs, mag
+    # plt.figure()
+    # plt.imshow( rgb_img )
+    # plt.show(block=False)
+    
+    return color_rectangle, H, V
 
-
+###################################################################################################
 
 def test_resize_complex_image():
 
@@ -92,13 +123,15 @@ def test_resize_complex_image():
         cv.INTER_LANCZOS4
     ]
 
-    #=================================================================================
-    # create a complex valued array with somewhat realistic x-ray beam phase structure
+    #=====================================
+    # create a random complex valued array
+    #=====================================
 
     phi = ( 2 * np.random.rand( nxny[1], nxny[0] ).astype( np.float32 ) - 1 ) + 1j * ( 2 * np.random.rand( nxny[1], nxny[0] ).astype( np.float32 ) - 1 )
 
     #=================================================================
     # resize phi using different built in openCV interpolation methods
+    #=================================================================
 
     final_size_xy = (int(nxny[0] * resize_factor_xy[0]),
                      int(nxny[1] * resize_factor_xy[1]))
@@ -111,6 +144,7 @@ def test_resize_complex_image():
 
     #===============================================
     # resize phi using function default combinations
+    #===============================================
 
     imgRSx = tike.view.resize_complex_image(phi)
     assert( imgRSx.shape[0] == nxny[1])
@@ -129,46 +163,63 @@ def test_resize_complex_image():
     assert( imgRSx.shape[0] == final_size_xy[1])
     assert( imgRSx.shape[1] == final_size_xy[0])
     assert( imgRSx.dtype == phi.dtype )
-    
-def test_complexHSV_to_RGB_colorwheel_colorrectangle():
 
-    #==========================================================
-    # create colorwheel for the "ground truth" comparison below
+###################################################################################################
+
+def test_complexHSV_to_RGB_color_wheel_color_rectangle():
+
+    #============================================================================
+    # create color wheel over full +/- pi for the "ground truth" comparison below
+    #============================================================================
 
     nxny = ( 512, 511 ) 
-    colorwheel, H, V = create_colorwheel( nxny )
+    colorwheel, H, V = create_color_wheel( nxny )
 
     # convert color wheel to complex representation, then convert back to HSV and then to RGB:
-    colorwheel2 = V * np.exp( 1j * H + 0 * 1j * np.pi / 1 )
+    colorwheel2 = V * np.exp( 1j * H )
     colorwheel2 = tike.view.complexHSV_to_RGB( colorwheel2 )
 
-    # plt.figure()
-    # plt.imshow( colorwheel )
-    # plt.show(block=False)
+    fig, axs = plt.subplots(1, 3)
+    axs[0].imshow( colorwheel2[:,:,0] - colorwheel[:,:,0] )
+    axs[0].title.set_text('Red Error')
+    axs[1].imshow( colorwheel2[:,:,1] - colorwheel[:,:,1] )
+    axs[1].title.set_text('Green Error')
+    axs[2].imshow( colorwheel2[:,:,2] - colorwheel[:,:,2] )
+    axs[2].title.set_text('Blue Error')
+    plt.show(block=False)
 
-    # plt.figure()
-    # plt.imshow( colorwheel2 )
-    # plt.show(block=False)
+    # Error to third decimal place is good enough for by-eye visual inspection purposes
+    np.testing.assert_array_almost_equal( colorwheel2, colorwheel, decimal = 3 )
 
-    # fig, axs = plt.subplots(1, 3)
-    # axs[0].imshow( colorwheel2[:,:,0] - colorwheel[:,:,0] )
-    # axs[0].title.set_text('Red Error')
-    # axs[1].imshow( colorwheel2[:,:,1] - colorwheel[:,:,1] )
-    # axs[1].title.set_text('Green Error')
-    # axs[2].imshow( colorwheel2[:,:,2] - colorwheel[:,:,2] )
-    # axs[2].title.set_text('Blue Error')
-    # plt.show(block=False)
+    #====================================================================================
+    # create color wheel over smaller phase range for the "ground truth" comparison below
+    #====================================================================================
 
-    # np.sum( colorwheel2 - colorwheel ) / np.sum( colorwheel )
+    nxny = ( 512, 511 ) 
+    colorwheel, H, V = create_color_wheel( nxny, -np.pi/3.0, +np.pi/2.0 )
 
-    # Error to second decimal place is good enough for by-eye visual inspection purposes
-    np.testing.assert_array_almost_equal( colorwheel2, colorwheel, decimal = 2 )
+    # convert color wheel to complex representation, then convert back to HSV and then to RGB:
+    colorwheel2 = V * np.exp( 1j * H )
+    colorwheel2 = tike.view.complexHSV_to_RGB( colorwheel2 )
 
-    #===============================================================
-    # create color rectangle for the "ground truth" comparison below
+    fig, axs = plt.subplots(1, 3)
+    axs[0].imshow( colorwheel2[:,:,0] - colorwheel[:,:,0] )
+    axs[0].title.set_text('Red Error')
+    axs[1].imshow( colorwheel2[:,:,1] - colorwheel[:,:,1] )
+    axs[1].title.set_text('Green Error')
+    axs[2].imshow( colorwheel2[:,:,2] - colorwheel[:,:,2] )
+    axs[2].title.set_text('Blue Error')
+    plt.show(block=False)
+
+    # Error to third decimal place is good enough for by-eye visual inspection purposes
+    np.testing.assert_array_almost_equal( colorwheel2, colorwheel, decimal = 3 )
+
+    #================================================================================
+    # create color rectangle over full +/- pi for the "ground truth" comparison below
+    #================================================================================
 
     sxsy = ( 201, 101 ) 
-    color_rectangle, phs, mag = create_colorrectangle( sxsy )
+    color_rectangle, phs, mag = create_color_rectangle( sxsy )
 
     # convert color rectangle to complex representation, then convert back to HSV and then to RGB:
     color_rectangle2 = mag * np.exp( 1j * phs )
@@ -182,13 +233,71 @@ def test_complexHSV_to_RGB_colorwheel_colorrectangle():
     # plt.imshow( color_rectangle2 )
     # plt.show(block=False)
 
-    np.testing.assert_array_equal( color_rectangle2, color_rectangle )
+    fig, axs = plt.subplots(1, 3)
+    axs[0].imshow( color_rectangle2[:,:,0] - color_rectangle[:,:,0] )
+    axs[0].title.set_text('Red Error')
+    axs[1].imshow( color_rectangle2[:,:,1] - color_rectangle[:,:,1] )
+    axs[1].title.set_text('Green Error')
+    axs[2].imshow( color_rectangle2[:,:,2] - color_rectangle[:,:,2] )
+    axs[2].title.set_text('Blue Error')
+    plt.show(block=False)
 
-#==================================================================================================
-#==================================================================================================
+    np.testing.assert_array_almost_equal( color_rectangle2, color_rectangle, decimal = 3 )
+
+    #========================================================================================
+    # create color rectangle over smaller phase range for the "ground truth" comparison below
+    #========================================================================================
+
+    sxsy = ( 201, 101 ) 
+    color_rectangle, phs, mag = create_color_rectangle( sxsy, -np.pi/3, +np.pi/2 )
+
+    # convert color rectangle to complex representation, then convert back to HSV and then to RGB:
+    color_rectangle2 = mag * np.exp( 1j * phs )
+    color_rectangle2 = tike.view.complexHSV_to_RGB( color_rectangle2 )
+
+    # plt.figure()
+    # plt.imshow( color_rectangle )
+    # plt.show(block=False)
+
+    # plt.figure()
+    # plt.imshow( color_rectangle2 )
+    # plt.show(block=False)
+
+    fig, axs = plt.subplots(1, 3)
+    axs[0].imshow( color_rectangle2[:,:,0] - color_rectangle[:,:,0] )
+    axs[0].title.set_text('Red Error')
+    axs[1].imshow( color_rectangle2[:,:,1] - color_rectangle[:,:,1] )
+    axs[1].title.set_text('Green Error')
+    axs[2].imshow( color_rectangle2[:,:,2] - color_rectangle[:,:,2] )
+    axs[2].title.set_text('Blue Error')
+    plt.show(block=False)
+
+    np.testing.assert_array_almost_equal( color_rectangle2, color_rectangle, decimal = 3 )
+
+###################################################################################################
 
 if __name__ == '__main__':
 
     test_resize_complex_image()
-    test_complexHSV_to_RGB_colorwheel_colorrectangle()
+    test_complexHSV_to_RGB_color_wheel_color_rectangle()
+
+    #================================
+    # test if complex array is not 2D
+    #================================
+
+    sz = ( 4, 7, 6 )
+    phi = ( 2 * np.random.rand( *sz ).astype( np.float32 ) - 1 ) + 1j * ( 2 * np.random.rand( *sz ).astype( np.float32 ) - 1 )
+
+    phi_RGB = tike.view.complexHSV_to_RGB( phi )
+
+    assert( phi_RGB.shape[0] == sz[0])
+    assert( phi_RGB.shape[1] == sz[1])
+    assert( phi_RGB.shape[2] == sz[2])
+    assert( phi_RGB.shape[3] == 3 )
+
+    plt.figure()
+    plt.imshow( phi_RGB[:,:,2,:] )
+    plt.show(block=False)
+
+
 