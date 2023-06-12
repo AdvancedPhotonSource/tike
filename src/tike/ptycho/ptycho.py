f423eb13e8d5a1e5965aa3ce4d7b8e91cac0d32b
Daniel Ching <carterbox@users.noreply.github.com>
2023-06-12 15:37:33 -0500

    NEW: Trim memory pool at the end of reconstruction

--- src/tike/ptycho/ptycho.py
+++ src/tike/ptycho/ptycho.py
@@ -525,9 +525,14 @@ class Reconstruction():
 
     def __exit__(self, type, value, traceback):
         self._get_result()
+        self._device_parameters = None
         self.comm.__exit__(type, value, traceback)
         self.operator.__exit__(type, value, traceback)
         self.device.__exit__(type, value, traceback)
+        mempool = cp.get_default_memory_pool()
+        mempool.free_all_blocks()
+        pinned_mempool = cp.get_default_pinned_memory_pool()
+        pinned_mempool.free_all_blocks()
 
     def get_convergence(
         self
