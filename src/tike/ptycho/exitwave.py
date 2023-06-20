e729c4b18d44f24b4d55ce7fafc872c646f08950
Ashish Tripathi <atripath@axinite.xray.aps.anl.gov>
2023-06-20 08:29:48 -0500

    .

--- src/tike/ptycho/exitwave.py
+++ src/tike/ptycho/exitwave.py
@@ -1,5 +1,9 @@
-"""
-TODO: fill this part in describing what's happening here
+"""Functions related to creating and manipulating ptychographic exitwave arrays.
+
+Ptychographic exitwaves are stored as a single complex array which represent
+the wavefield after any and all interaction with the sample and thus there's
+just free space propagation to the detector.
+
 """
 
 from __future__ import annotations
@@ -123,82 +127,3 @@ def poisson_steplength_ptychoshelves( xi,
     return step_length
 
 ###################################################################################################
-
-# def poisson_steplength_exact( xi, 
-#                               abs2_Psi, 
-#                               I_e, 
-#                               I_m, 
-#                               measured_pixels, 
-#                               step_length_test ):
-                              
-#     if measured_pixels.size == 0:
-#         measured_pixels = 1
-
-#     xi_abs_Psi2        = measured_pixels * xi * abs2_Psi               
-#     xi_alpha_minus_one = xi * step_length_test[ ..., None, None, None ] - 1   # cp.newaxis vs None ???   try:  cp.newaxis is None
-
-#     lhs = cp.sum( xi_abs_Psi2 * xi_alpha_minus_one, axis = (-1, -2 ) )  
-
-#     numer = I_m * xi_alpha_minus_one
-#     denom = abs2_Psi * cp.square( xi_alpha_minus_one ) + I_e - abs2_Psi
-
-#     rhs = cp.sum( xi_abs_Psi2 * ( numer / denom ), axis = (-1, -2 ) )   
-
-#     f_eq_0 = cp.abs( lhs - rhs )
-
-#     II = cp.argmin( f_eq_0, axis = 0 )
-
-#     step_length = cp.zeros( ( abs2_Psi.shape[0], abs2_Psi.shape[1] ), dtype = 'float32' )      
-
-#     for pp in cp.arange(0, Nscpm ):
-#         step_length[ pp, : ] = step_length_test[ II[ pp, : ], pp ]
-
-#     return step_length, f_eq_0
-  
-###################################################################################################
-
-# if __name__ == "__main__":
-
-    # import h5py
-
-    # f = h5py.File( '/net/s8iddata/export/8-id-ECA/Analysis/atripath/Python/poisson_exwv/pythonTIKE_poisson_testing_31May2023.mat', 'r' )
-
-    # Nspos = int( np.ndarray.item( np.array( f['Nspos'] )))    
-    # Nscpm = int( np.ndarray.item( np.array( f['Nscpm'] ))) 
-
-    # sz_Nr = 256
-    # sz_Nc = 384
-
-    # xi       = cp.array( f['xi'] ).reshape( ( Nspos, sz_Nr, sz_Nc ), order='F')           # = 1 - I_m / I_e
-    # abs2_Psi = cp.array( f['abs2_Psi'] ).reshape( ( Nscpm, Nspos, sz_Nr, sz_Nc ), order='F')
-    # I_e      = cp.array( f['I_e'] ).reshape( ( Nspos, sz_Nr, sz_Nc ), order='F')
-    # I_m      = cp.array( f['I_m'] ).reshape( ( Nspos, sz_Nr, sz_Nc ), order='F')
-
-    # measured_pixels = cp.array( f['not_I_m_eq0'] ).reshape( ( sz_Nr, sz_Nc ), order='F')
-    # alpha_test      = cp.squeeze( cp.array( f['alpha_test'] ))
- 
-    # #=====
-
-    # poiss_steplength_exact, f_eq_0 = poisson_steplength_exact( xi, abs2_Psi, I_e, I_m, measured_pixels, alpha_test )
-    # poiss_steplength_approx        = poisson_steplength_approx( xi, abs2_Psi, I_e, I_m, measured_pixels, 0.50 * cp.ones( ( Nscpm, Nspos )), 0.70 )
-    # poiss_steplength_ptychoshelves = poisson_steplength_ptychoshelves( xi, I_e, I_m, measured_pixels, 0.5 * cp.ones( Nspos ), 0.50 )
-
-    # #=====
-
-    # poiss_steplength_exact         = cp.asnumpy( poiss_steplength_exact )
-    # poiss_steplength_approx        = cp.asnumpy( poiss_steplength_approx )
-    # poiss_steplength_ptychoshelves = cp.asnumpy( poiss_steplength_ptychoshelves )
-    # alpha_test                     = cp.asnumpy( alpha_test )
-    # f_eq_0                         = cp.asnumpy( f_eq_0 )
-
-    # import matplotlib.pyplot as plt
-
-    # for pp in np.arange( 0, Nscpm ):
-    #     plt.subplot( 1, Nscpm, pp + 1 )
-    #     ax = plt.gca()
-    #     ax.plot( poiss_steplength_exact[ pp, : ], 0.5 + np.arange(0,Nspos), marker='x', color="red")
-    #     ax.plot(  np.squeeze( poiss_steplength_ptychoshelves ), 0.5 + np.arange(0,Nspos), marker='.', color="blue")
-    #     ax.plot(  poiss_steplength_approx[ pp, : ], 0.5 + np.arange(0,Nspos), marker='.', color="white")
-    #     ax.imshow( np.log10(f_eq_0[ :, pp, : ].T), interpolation='none', extent = [ np.min( alpha_test[:,pp] ), np.max( alpha_test[:,pp] ), Nspos, 0 ], aspect="auto" )
-        
-    # plt.show( block = False )
