7fb47c6f6ce9409f175ea6fa6a181eb2b6300410
Daniel Ching <carterbox@users.noreply.github.com>
2024-05-29 16:01:02 -0500

    TST: Fixup single slice test

--- tests/operators/test_multislice.py
+++ tests/operators/test_multislice.py
@@ -5,8 +5,7 @@ import time
 import unittest
 
 import numpy as np
-from tike.operators import Multislice
-from tike.operators.cupy.multislice import SingleSlice
+from tike.operators import Multislice, SingleSlice
 import tike.precision
 import tike.linalg
 
@@ -17,7 +16,7 @@ __copyright__ = "Copyright (c) 2020, UChicago Argonne, LLC."
 __docformat__ = 'restructuredtext en'
 
 
-class TestMultislice(unittest.TestCase):
+class TestMultiSlice(unittest.TestCase):
     """Test the ptychography operator."""
 
     def setUp(self, depth=7, pw=15, nscan=27):
@@ -55,16 +54,13 @@ class TestMultislice(unittest.TestCase):
         }
         self.dkwargs = {
             'nearplane': self.xp.asarray(farplane),
-            'probe': self.xp.asarray(probe),
-            'psi': self.xp.asarray(original),
-            'scan': self.xp.asarray(scan),
         }
 
     def test_adjoint(self):
         """Check that the adjoint operator is correct."""
         d = self.operator.fwd(**self.mkwargs)
         assert d.shape == self.dkwargs['nearplane'].shape
-        m0, m1 = self.operator.adj(**self.dkwargs)
+        m0, m1 = self.operator.adj(**self.dkwargs, **self.mkwargs)
         assert m0.shape == self.mkwargs['psi'].shape
         assert m1.shape == self.mkwargs['probe'].shape
         a = tike.linalg.inner(d, self.dkwargs['nearplane'])
@@ -74,15 +70,17 @@ class TestMultislice(unittest.TestCase):
         print('<Fm,    d> = {:.5g}{:+.5g}j'.format(a.real.item(), a.imag.item()))
         print('< m0, F*d> = {:.5g}{:+.5g}j'.format(b.real.item(), b.imag.item()))
         print('< m1, F*d> = {:.5g}{:+.5g}j'.format(c.real.item(), c.imag.item()))
-        # self.xp.testing.assert_allclose(a.real, b.real, rtol=1e-3, atol=0)
-        # self.xp.testing.assert_allclose(a.imag, b.imag, rtol=1e-3, atol=0)
+        self.xp.testing.assert_allclose(a.real, b.real, rtol=1e-3, atol=0)
+        self.xp.testing.assert_allclose(a.imag, b.imag, rtol=1e-3, atol=0)
+        self.xp.testing.assert_allclose(a.real, c.real, rtol=1e-3, atol=0)
+        self.xp.testing.assert_allclose(a.imag, c.imag, rtol=1e-3, atol=0)
 
     @unittest.skip('FIXME: This operator is not scaled.')
     def test_scaled(self):
         pass
 
 
-class TestSingleslice(unittest.TestCase):
+class TestSingleSlice(TestMultiSlice):
     """Test the ptychography operator."""
 
     def setUp(self, depth=1, pw=15, nscan=27):
@@ -102,7 +100,7 @@ class TestSingleslice(unittest.TestCase):
         original = random_complex(*self.original_shape)
         farplane = random_complex(*self.probe_shape[:-2], *self.detector_shape)
 
-        self.operator = SingleSlice(
+        self.operator = Multislice(
             nscan=self.scan_shape[-2],
             probe_shape=self.probe_shape[-1],
             detector_shape=self.detector_shape[-1],
@@ -119,29 +117,8 @@ class TestSingleslice(unittest.TestCase):
             'scan': self.xp.asarray(scan),
         }
         self.dkwargs = {
-            'nearplane': self.xp.asarray(farplane)
+            'nearplane': self.xp.asarray(farplane),
         }
-
-    def test_adjoint(self):
-        """Check that the adjoint operator is correct."""
-        d = self.operator.fwd(**self.mkwargs)
-        assert d.shape == self.dkwargs['nearplane'].shape
-        m0, m1 = self.operator.adj(**self.mkwargs, **self.dkwargs)
-        assert m0.shape == self.mkwargs['psi'].shape
-        assert m1.shape == self.mkwargs['probe'].shape
-        a = tike.linalg.inner(d, self.dkwargs['nearplane'])
-        b = tike.linalg.inner(self.mkwargs['psi'], m0)
-        c = tike.linalg.inner(self.mkwargs['probe'], m1)
-        print()
-        print('<Fm,  m0> = {:.5g}{:+.5g}j'.format(a.real.item(), a.imag.item()))
-        print('<Fm,  m1> = {:.5g}{:+.5g}j'.format(c.real.item(), c.imag.item()))
-        print('< d, F*d> = {:.5g}{:+.5g}j'.format(b.real.item(), b.imag.item()))
-        self.xp.testing.assert_allclose(a.real, b.real, rtol=1e-3, atol=0)
-        self.xp.testing.assert_allclose(a.imag, b.imag, rtol=1e-3, atol=0)
-
-    @unittest.skip('FIXME: This operator is not scaled.')
-    def test_scaled(self):
-        pass
 
 
 if __name__ == '__main__':
