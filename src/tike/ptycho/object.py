03c971aebcc7e11f81afd9916cfb17c3f55c09a7
Daniel Ching <carterbox@users.noreply.github.com>
2024-07-24 17:51:19 -0500

    BUG: Avoid casting complex to real values

--- src/tike/ptycho/object.py
+++ src/tike/ptycho/object.py
@@ -89,12 +89,16 @@ class ObjectOptions:
         if self.v is not None:
             options.v = cp.asarray(
                 self.v,
-                dtype=tike.precision.floating,
+                dtype=tike.precision.cfloating
+                if np.iscomplexobj(self.v)
+                else tike.precision.floating,
             )
         if self.m is not None:
             options.m = cp.asarray(
                 self.m,
-                dtype=tike.precision.floating,
+                dtype=tike.precision.cfloating
+                if np.iscomplexobj(self.m)
+                else tike.precision.floating,
             )
         if self.preconditioner is not None:
             options.preconditioner = cp.asarray(
